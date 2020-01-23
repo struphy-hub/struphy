db38091b0edebe01cc2e13c3149bd7f1ed5993d7
flohold <florian.holderied@tum.de>
2020-01-23 10:27:34 +0100

    .

---
+++ ECHO_firstTest.py
@@ -0,0 +1,311 @@
+import numpy             as np
+import matplotlib.pyplot as plt
+import scipy.sparse      as sparse
+
+import utilitis_FEEC.bsplines       as bsp
+import utilitis_FEEC.projectors_mhd as mhd
+import utilitis_FEEC.projectors     as proj
+import utilitis_FEEC.derivatives    as der
+import utilitis_FEEC.evaluation     as eva
+
+import utilitis_FEEC.mass_matrices  as mass
+import utilitis_FEEC.mappings       as maps
+
+
+import utilitis_PIC.ECHO_fields
+import utilitis_PIC.ECHO_pusher
+import utilitis_PIC.ECHO_accumulation
+
+import sobol_seq
+import scipy.special as sp
+
+import time
+
+#====================================================================================
+#  calling epyccel for particle pusher
+#====================================================================================
+from pyccel.epyccel import epyccel
+
+pic_fields = epyccel(utilitis_PIC.ECHO_fields,       accelerator='openmp')
+pic_pusher = epyccel(utilitis_PIC.ECHO_pusher,       accelerator='openmp')
+pic_accumu = epyccel(utilitis_PIC.ECHO_accumulation, accelerator='openmp')
+
+print('pyccelization done!')
+#====================================================================================
+
+
+
+# ======================================= parameters ===================================================================
+Nel = [4, 4, 32]           # mesh generation on logical domain
+bc  = [True, True, True]   # boundary conditions
+p   = [2, 2, 2]            # splines degrees  
+L   = [1., 1., 2*np.pi]    # box lengthes of physical domain
+
+
+el_b      = [np.linspace(0., 1., Nel + 1) for Nel in Nel]                      # element boundaries
+delta     = [1/Nel for Nel in Nel]                                             # element sizes
+T         = [bsp.make_knots(el_b, p, bc) for el_b, p, bc in zip(el_b, p, bc)]  # knot vectors
+t         = [T[1:-1] for T in T]                                               # reduced knot vectors
+Nbase0    = [Nel + p - bc*p for Nel, p, bc in zip(Nel, p, bc)]                 # number of basis functions in V0
+Nbase_old = [Nel + p for Nel, p, bc in zip(Nel, p, bc)]                        # TODO delete this later
+Ntot      = Nbase0[0] *Nbase0[1]*Nbase0[2]                                     # total number of basis functions
+
+
+
+# geometry
+DF      = np.array([[  L[0], 0., 0.], [0.,   L[1], 0.], [0., 0.,   L[2]]])           # Jacobian matrix
+DFinv   = np.array([[1/L[0], 0., 0.], [0., 1/L[1], 0.], [0., 0., 1/L[2]]])           # inverse Jacobian matrix
+
+G       = np.array([[  L[0]**2, 0., 0.], [0.,   L[1]**2, 0.], [0., 0.,   L[2]**2]])  # metric tensor
+Ginv    = np.array([[1/L[0]**2, 0., 0.], [0., 1/L[1]**2, 0.], [0., 0., 1/L[2]**2]])  # inverse metric tensor
+
+g_sqrt  = L[0]*L[1]*L[2]                                                             # Jacobian determinant
+
+mapping = maps.mappings(['slab', L[0], L[1], L[2]])                                  # object for mappings in MHD part
+
+
+# time step
+dt = 0.05
+
+# particle parameters
+Np  = int(5e5)         # total number of particles
+vth = 1.               # thermal velocity of particles in all directions
+v0  = 2.               # mean velocity of particles in z-direction
+nuh = 0.05             # ratio of hot/cold mass densities        
+nh0 = (1.)*nuh*g_sqrt  # hot ion number density on logical domain
+
+# =====================================================================================================================
+
+
+# ===================== coefficients for pp-forms in interval [0, delta] (N and D) ====================================
+pp0 = []
+pp1 = []
+
+for i in range(3):
+    if p[i] == 3:
+        pp0.append(np.asfortranarray([[1/6, -1/(2*delta[i]), 1/(2*delta[i]**2), -1/(6*delta[i]**3)], [2/3, 0., -1/delta[i]**2, 1/(2*delta[i]**3)], [1/6, 1/(2*delta[i]), 1/(2*delta[i]**2), -1/(2*delta[i]**3)], [0., 0., 0., 1/(6*delta[i]**3)]]))
+        pp1.append(np.asfortranarray([[1/2, -1/delta[i], 1/(2*delta[i]**2)], [1/2, 1/delta[i], -1/delta[i]**2], [0., 0., 1/(2*delta[i]**2)]])/delta[i])
+    elif p[i] == 2:
+        pp0.append(np.asfortranarray([[1/2, -1/delta[i], 1/(2*delta[i]**2)], [1/2, 1/delta[i], -1/delta[i]**2], [0., 0., 1/(2*delta[i]**2)]]))
+        pp1.append(np.asfortranarray([[1., -1/delta[i]], [0., 1/delta[i]]])/delta[i])
+    else:
+        print('So far only cubic and quadratic splines implemented!')
+# =====================================================================================================================
+
+
+# ====================================== background quantities ========================================================
+Ueq_phys = np.array([0., 0., 0.])     # background bulk flow (vector/1-form on physical domain)
+Ueq      = DF.T.dot(Ueq_phys)         # background bulk flow (1-form on logical domain)
+
+
+Beq_phys = np.array([0., 0., 1.])     # background magnetic field (vector/2-form on physical domain)
+Beq      = g_sqrt*DFinv.dot(Beq_phys) # background magnetic field (2-form on logical domain)
+
+B0_23    = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[0][0](q1, q2, q3) * (0.)
+B0_31    = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[1][1](q1, q2, q3) * (0.)   
+B0_12    = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[2][2](q1, q2, q3) * (1.)   
+
+B0_hat   = [B0_23, B0_31, B0_12]
+
+rho0_123 = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * (1.) # background bulk mass density (3-form on logical domain)
+# =====================================================================================================================
+
+
+# ============================================== initial conditions ===================================================
+k   = 1.    # wavenumber of initial perturbation
+amp = 1e-5  # amplitude  of initial perturbation
+
+B1_ini = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[0][0](q1, q2, q3) * (amp * np.sin(k * q3 *L[2]))
+B2_ini = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[1][1](q1, q2, q3) * (0. * q1)
+B3_ini = lambda q1, q2, q3 : mapping.g_sqrt(q1, q2, q3) * mapping.DFinv[2][2](q1, q2, q3) * (0. * q1)
+
+U1_ini = lambda q1, q2, q3 : mapping.DF[0][0](q1, q2, q3) * (0. * q1)  # actually DF.T !!
+U2_ini = lambda q1, q2, q3 : mapping.DF[1][1](q1, q2, q3) * (0. * q1)  # actually DF.T !!
+U3_ini = lambda q1, q2, q3 : mapping.DF[2][2](q1, q2, q3) * (0. * q1)  # actually DF.T !!
+# =====================================================================================================================
+
+
+# ========================================= reserve memory for unknowns ===============================================
+b     = np.empty(3*Ntot, dtype=float)   # B-field FEM coefficients
+u     = np.empty(3*Ntot, dtype=float)   # U-field FEM coefficients
+u_old = np.empty(3*Ntot, dtype=float)   # U-field FEM coefficients from previous time step (needed in step 3)
+
+# matrices and vectors in steps 1 and 3
+mat11 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+mat12 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+mat13 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+
+mat22 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+mat23 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+mat33 = np.empty((Nbase0[0], Nbase0[1], Nbase0[2], Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+
+vec1  = np.empty((Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+vec2  = np.empty((Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+vec3  = np.empty((Nbase0[0], Nbase0[1], Nbase0[2]), dtype=float, order='F')
+
+# particles and span indices
+particles = np.empty((Np, 7), dtype=float, order='F')
+spans0    = np.empty((Np, 3), dtype=int,   order='F')
+
+# fields at particle positions
+B_part = np.empty((Np, 3), dtype=float, order='F')
+U_part = np.empty((Np, 3), dtype=float, order='F')
+# =====================================================================================================================
+
+
+# ======================================== projection of initial conditions ===========================================
+# create object for projecting initial conditions
+PRO = proj.projectors_3d(p, Nbase_old, T, bc)
+
+# left-hand sides of projectors
+PRO.assemble_V1()
+PRO.assemble_V2()
+
+# projection of initial conditions
+b[0*Ntot:1*Ntot], b[1*Ntot:2*Ntot], b[2*Ntot:3*Ntot] = PRO.PI_2([B1_ini, B2_ini, B3_ini])
+u[0*Ntot:1*Ntot], u[1*Ntot:2*Ntot], u[2*Ntot:3*Ntot] = PRO.PI_1([U1_ini, U2_ini, U3_ini])
+
+
+print('projection of initial conditions done')
+# =====================================================================================================================
+
+
+# ============================================ MHD matrices ===========================================================
+# create object for projecting MHD matrices
+MHD = mhd.projections_mhd(p, Nbase_old, T, bc)
+
+# right-hand side of projection matrices
+W1,   W2,   W3   = MHD.projection_W(rho0_123, mapping.g_sqrt)
+TAU1, TAU2, TAU3 = MHD.projection_T(B0_hat, mapping.Ginv)
+
+# mass matrices in V1 and V2
+M1 = mass.mass_V1(T, p, bc, mapping.Ginv, mapping.g_sqrt)
+M2 = mass.mass_V2(T, p, bc, mapping.G, mapping.g_sqrt)
+
+# discrete curl and div matrices
+derivatives = der.discrete_derivatives(p, T, bc)
+
+CURL = derivatives.CURL_3d(T, p, bc)
+DIV  = derivatives.DIV_3d(T, p, bc)
+
+# perform projections of TAU1, TAU2, TAU3
+TAU1 = sparse.linalg.spsolve(PRO.interhistopolation_V1_1, TAU1)
+TAU2 = sparse.linalg.spsolve(PRO.interhistopolation_V1_2, TAU2)
+TAU3 = sparse.linalg.spsolve(PRO.interhistopolation_V1_3, TAU3)
+
+TAU  = sparse.bmat([[TAU1], [TAU2], [TAU3]], format='csc')
+
+# perform projections of W1, W2, W3
+W1   = sparse.linalg.spsolve(PRO.interhistopolation_V1_1, W1)
+W2   = sparse.linalg.spsolve(PRO.interhistopolation_V1_2, W2)
+W3   = sparse.linalg.spsolve(PRO.interhistopolation_V1_3, W3)
+
+W    = sparse.bmat([[W1, None, None], [None, W2, None], [None, None, W3]], format='csc')
+
+# compute matrix A
+A = 1/2*(M1.dot(W) + W.T.dot(M1))
+
+
+# LU decompostion of Schur complement in step 2
+STEP2_schur_LU = sparse.linalg.splu((A + dt**2/4*TAU.T.dot(CURL.T.dot(M2.dot(CURL.dot(TAU))))).tocsc())
+
+# other matrices needed in step2
+STEP2_1 = (A - dt**2/4*TAU.T.dot(CURL.T.dot(M2.dot(CURL.dot(TAU))))).tocsc()
+STEP2_2 = dt*TAU.T.dot(CURL.T.dot(M2)).tocsc()
+
+A = A.toarray()
+
+# delete everything which is not needed to save memory
+del PRO, MHD, W1, W2, W3, TAU1, TAU2, TAU3, M1, W
+
+print('Assembly of constant matrices done')
+# ======================================================================================================================
+
+
+
+
+# ================================ create particles with random numbers ================================================
+#particles[:, :6] = sobol_seq.i4_sobol_generate(6, Np)
+particles[:, :6] = np.random.rand(Np, 6)
+
+# transform velocities to Maxwellians
+particles[:, 3]  = sp.erfinv(2*particles[:, 3] - 1)*vth
+particles[:, 4]  = sp.erfinv(2*particles[:, 4] - 1)*vth
+particles[:, 5]  = sp.erfinv(2*particles[:, 5] - 1)*vth + v0
+
+# compute weights
+particles[:, 6]  = nh0/Np 
+
+# compute spans
+spans0[:, 0] = np.floor(particles[:, 0]*Nel[0]).astype(int) + p[0]
+spans0[:, 1] = np.floor(particles[:, 1]*Nel[1]).astype(int) + p[1]
+spans0[:, 2] = np.floor(particles[:, 2]*Nel[2]).astype(int) + p[2]
+
+print('Particle initialization done')
+# =====================================================================================================================
+
+
+# compute initial fields at particle positions
+timea = time.time()
+pic_fields.evaluate_2form(particles[:, 0:3], p, spans0, Nbase0, Np, np.asfortranarray(b[:Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[Ntot:2*Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[2*Ntot:].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), Beq, pp0[0], pp0[1], pp0[2], pp1[0], pp1[1], pp1[2], B_part)
+pic_fields.evaluate_1form(particles[:, 0:3], p, spans0, Nbase0, Np, np.asfortranarray(u[:Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(u[Ntot:2*Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(u[2*Ntot:].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), Ueq, pp0[0], pp0[1], pp0[2], pp1[0], pp1[1], pp1[2], U_part)
+timeb = time.time()
+print('Initial field computation at particles done. Time : ', timeb-timea)
+
+# initial energies
+en_U = 1/2*u.dot(A.dot(u))
+en_B = 1/2*b.dot(M2.dot(b))
+en_H = 1/2*particles[:, 6].dot(particles[:, 3]**2 + particles[:, 4]**2 + particles[:, 5]**2)
+
+energies = np.array([en_U, en_B, en_H])
+
+
+
+def update():
+    
+    # step 1 (update u)
+    pic_accumu.accumulation_step1(particles, p, spans0, Nbase0, T[0], T[1], T[2], t[0], t[1], t[2], L, B_part, mat12, mat13, mat23)
+    
+    AJ11A = np.block([[np.zeros((Ntot, Ntot)), mat12.reshape(Ntot, Ntot), mat13.reshape(Ntot, Ntot)], [-mat12.reshape(Ntot, Ntot).T, np.zeros((Ntot, Ntot)), mat23.reshape(Ntot, Ntot)], [-mat13.reshape(Ntot, Ntot).T, -mat23.reshape(Ntot, Ntot).T, np.zeros((Ntot, Ntot))]])
+    
+    u[:] = np.linalg.solve(A - dt/2*AJ11A, (A + dt/2*AJ11A).dot(u))
+    
+    
+    # step 2 (update first u, then b and evaluate B-field at particle positions)
+    u_old[:] = u
+    
+    u[:] = STEP2_schur_LU.solve(STEP2_1.dot(u_old) + STEP2_2.dot(b))
+    b[:] = b - dt/2*CURL.dot(TAU.dot(u_old + u))
+    
+    pic_fields.evaluate_2form(particles[:, 0:3], p, spans0, Nbase0, Np, np.asfortranarray(b[:Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[Ntot:2*Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[2*Ntot:].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), Beq, pp0[0], pp0[1], pp0[2], pp1[0], pp1[1], pp1[2], B_part)
+    
+    
+    # step 3 (update first u, then evaluate U-field at particle positions and then update V)
+    pic_accumu.accumulation_step3(particles, p, spans0, Nbase0, T[0], T[1], T[2], t[0], t[1], t[2], L, B_part, mat11, mat12, mat13, mat22, mat23, mat33, vec1, vec2, vec3)
+    
+    BLOCK = np.block([[mat11.reshape(Ntot, Ntot), mat12.reshape(Ntot, Ntot), mat13.reshape(Ntot, Ntot)], [mat12.reshape(Ntot, Ntot).T, mat22.reshape(Ntot, Ntot), mat23.reshape(Ntot, Ntot)], [mat13.reshape(Ntot, Ntot).T, mat23.reshape(Ntot, Ntot).T, mat33.reshape(Ntot, Ntot)]])
+    
+    u_old[:] = u
+    
+    u[:] = np.linalg.solve(A + dt**2/4*BLOCK, (A - dt**2/4*BLOCK).dot(u_old) + dt*np.concatenate((vec1.flatten(), vec2.flatten(), vec3.flatten())))
+    
+    pic_fields.evaluate_1form(particles[:, 0:3], p, spans0, Nbase0, Np, np.asfortranarray(1/2*(u + u_old)[:Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(1/2*(u + u_old)[Ntot:2*Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(1/2*(u + u_old)[2*Ntot:].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), Ueq, pp0[0], pp0[1], pp0[2], pp1[0], pp1[1], pp1[2], U_part)
+    
+    pic_pusher.pusher_step3(particles, L, dt, B_part, U_part)
+    
+    # step 4 (update Q and spans)
+    pic_pusher.pusher_step4(particles, L, dt)
+    
+    spans0[:, 0] = np.floor(particles[:, 0]*Nel[0]).astype(int) + p[0]
+    spans0[:, 1] = np.floor(particles[:, 1]*Nel[1]).astype(int) + p[1]
+    spans0[:, 2] = np.floor(particles[:, 2]*Nel[2]).astype(int) + p[2]
+    
+    # step 5 (update V)
+    pic_fields.evaluate_2form(particles[:, 0:3], p, spans0, Nbase0, Np, np.asfortranarray(b[:Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[Ntot:2*Ntot].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), np.asfortranarray(b[2*Ntot:].reshape(Nbase0[0], Nbase0[1], Nbase0[2])), Beq, pp0[0], pp0[1], pp0[2], pp1[0], pp1[1], pp1[2], B_part)
+    pic_pusher.pusher_step5(particles, L, dt, B_part)
+    
+
+timea = time.time()
+update()
+timeb = time.time()
+print('time for one step : ', timeb-timea)
