c4ba0584b1da2243faaa88da1a8f2a29e4aea05f
flohold <florian.holderied@tum.de>
2020-04-16 09:20:21 +0200

    evaluation function added

--- hylife/geometry/mappings_analytical.py
+++ hylife/geometry/mappings_analytical.py
@@ -505,3 +505,119 @@ def g_inv(xi1, xi2, xi3, kind, params, component):
             value = (Lx*Ly * (1 + alpha * cos(2*pi*xi1) * sin(2*pi*xi2) * 2*pi + alpha * sin(2*pi*xi1) * cos(2*pi*xi2) * 2*pi))**2
           
     return value/det_df(xi1, xi2, xi3, kind, params)**2
+
+
+
+
+# ==========================================================================================
+@types('double','double','double','int','int','double[:]')        
+def fun(xi1, xi2, xi3, kind_fun, kind_map, params):
+    
+    value = 0.
+    
+    # mapping f
+    if   kind_fun == 1:
+        value = f(xi1, xi2, xi3, kind_map, params, 1)
+    elif kind_fun == 2:
+        value = f(xi1, xi2, xi3, kind_map, params, 2)
+    elif kind_fun == 3:
+        value = f(xi1, xi2, xi3, kind_map, params, 3)
+    
+    # Jacobian matrix df
+    elif kind_fun == 11:
+        value = df(xi1, xi2, xi3, kind_map, params, 11)
+    elif kind_fun == 12:
+        value = df(xi1, xi2, xi3, kind_map, params, 12)
+    elif kind_fun == 13:
+        value = df(xi1, xi2, xi3, kind_map, params, 13)
+    elif kind_fun == 14:
+        value = df(xi1, xi2, xi3, kind_map, params, 21)
+    elif kind_fun == 15:
+        value = df(xi1, xi2, xi3, kind_map, params, 22)
+    elif kind_fun == 16:
+        value = df(xi1, xi2, xi3, kind_map, params, 23)
+    elif kind_fun == 17:
+        value = df(xi1, xi2, xi3, kind_map, params, 31)
+    elif kind_fun == 18:
+        value = df(xi1, xi2, xi3, kind_map, params, 32)
+    elif kind_fun == 19:
+        value = df(xi1, xi2, xi3, kind_map, params, 33)
+        
+    # Jacobian determinant det_df
+    elif kind_fun == 4:
+        value = det_df(xi1, xi2, xi3, kind_map, params)
+        
+    # inverse Jacobian matrix df_inv
+    elif kind_fun == 21:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 11)
+    elif kind_fun == 22:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 12)
+    elif kind_fun == 23:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 13)
+    elif kind_fun == 24:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 21)
+    elif kind_fun == 25:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 22)
+    elif kind_fun == 26:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 23)
+    elif kind_fun == 27:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 31)
+    elif kind_fun == 28:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 32)
+    elif kind_fun == 29:
+        value = df_inv(xi1, xi2, xi3, kind_map, params, 33)
+        
+    # metric tensor g
+    elif kind_fun == 31:
+        value = g(xi1, xi2, xi3, kind_map, params, 11)
+    elif kind_fun == 32:
+        value = g(xi1, xi2, xi3, kind_map, params, 12)
+    elif kind_fun == 33:
+        value = g(xi1, xi2, xi3, kind_map, params, 13)
+    elif kind_fun == 34:
+        value = g(xi1, xi2, xi3, kind_map, params, 21)
+    elif kind_fun == 35:
+        value = g(xi1, xi2, xi3, kind_map, params, 22)
+    elif kind_fun == 36:
+        value = g(xi1, xi2, xi3, kind_map, params, 23)
+    elif kind_fun == 37:
+        value = g(xi1, xi2, xi3, kind_map, params, 31)
+    elif kind_fun == 38:
+        value = g(xi1, xi2, xi3, kind_map, params, 32)
+    elif kind_fun == 39:
+        value = g(xi1, xi2, xi3, kind_map, params, 33)
+        
+    # metric tensor g_inv
+    elif kind_fun == 41:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 11)
+    elif kind_fun == 42:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 12)
+    elif kind_fun == 43:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 13)
+    elif kind_fun == 44:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 21)
+    elif kind_fun == 45:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 22)
+    elif kind_fun == 46:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 23)
+    elif kind_fun == 47:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 31)
+    elif kind_fun == 48:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 32)
+    elif kind_fun == 49:
+        value = g_inv(xi1, xi2, xi3, kind_map, params, 33)
+    
+    return value
+
+
+
+
+
+# ==========================================================================================
+@types('int[:]','double[:]','double[:]','double[:]','double[:,:,:](order=F)','int','int','double[:]')        
+def kernel_eva(n, xi1, xi2, xi3, mat_f, kind_fun, kind_map, params):
+    
+    for i1 in range(n[0]):
+        for i2 in range(n[1]):
+            for i3 in range(n[2]):
+                mat_f[i1, i2, i3] = fun(xi1[i1], xi2[i2], xi3[i3], kind_fun, kind_map, params)
