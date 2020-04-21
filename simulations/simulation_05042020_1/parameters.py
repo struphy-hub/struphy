1806cf631beebb1c2f04e0596a9902387c221455
flohold <florian.holderied@tum.de>
2020-04-21 17:40:06 +0200

    new output procedure : now with hdf5 file

--- simulations/simulation_05042020_1/parameters.py
+++ simulations/simulation_05042020_1/parameters.py
@@ -5,32 +5,38 @@ class parameters():
     
     def __init__(self):
         
-        self.Nel          = [16, 4, 4]             # mesh generation on logical domain
+        self.Nel          = [16, 2, 2]             # mesh generation on logical domain
         self.bc           = [True, True, True]     # boundary conditions (True: periodic, False: else)
-        self.p            = [3, 2, 2]              # spline degrees  
+        self.p            = [3, 1, 1]              # spline degrees  
+        
+        self.nq_el        = [4, 2, 2] # number of quadrature points per element for integrations over whole domain
+        self.nq_pr        = [6, 6, 6] # number of quadrature points per integration interval of projectors
 
         self.time_int     = True     # do time integration?
         self.dt           = 0.06     # time step
-        self.Tend         = 201.     # simulation time
+        self.Tend         = 1.2     # simulation time
         self.max_time     = 60*60    # maximum runtime of program in minutes
         self.add_pressure = False    # add non-Hamiltonian terms to simulation?
 
         # geometry
         self.kind_map     =  1                         # 1 : slab, 2 : hollow cylinder, 3 : colella
+        
         self.params_map   = [2*np.pi/0.75, 1., 1.]     # parameters for mapping  
+        #self.params_map   = [2*np.pi/0.75, 1., 0.05, 1.]     # parameters for mapping  
         
         # physical constants
-        self.gamma        = 5/3                       # adiabatic exponent
+        self.gamma        = 5/3                        # adiabatic exponent
 
         # particle parameters
+        self.add_PIC      = True                # add kinetic terms to simulation?
         self.Np           = 128000              # total number of particles
-        self.control      = 1                   # control variate? (0: no, 1: yes)
+        self.control      = True                # control variate for noise resuction? (delta-f method)
 
-        self.v0x = 2.5
-        self.v0y = 0.
-        self.v0z = 0.
+        self.v0x = 2.5                      # shift of Maxwellian in vx-direction
+        self.v0y = 0.                       # shift of Maxwellian in vy-direction
+        self.v0z = 0.                       # shift of Maxwellian in vz-direction
         
-        self.vth = 1.
+        self.vth = 1.                       # hot ion thermal velocity
 
         # particle loading
         '''
@@ -44,7 +50,89 @@ class parameters():
 
 
         # Is this run a restart?
-        self.restart = False  
+        self.restart = False 
+        self.num_restart = 0
 
-        # Create restart files at the end of the simulation? If True, name full directory where to save them
-        self.create_restart = False
+        # Create restart files at the end of the simulation?
+        self.create_restart = True
+        
+        # initial conditions 
+        self.ic_from_params = False
+        
+        self.nmodes = 64
+        self.modes  = np.linspace(0, self.nmodes, self.nmodes + 1) - self.nmodes/2
+        self.modes  = np.delete(self.modes, int(self.nmodes/2))
+        
+         
+    def u1_ini(self, xi1, xi2, xi3):
+    
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def u2_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def u3_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def b1_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += 0*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def b2_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def b3_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+
+    def rho_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
+
+    def p_ini(self, xi1, xi2, xi3):
+
+        values = np.zeros(xi1.shape)
+
+        for i in range(self.nmodes):
+            values += np.random.rand()*np.sin(2*np.pi*self.modes[i]*xi1)
+
+        return values
