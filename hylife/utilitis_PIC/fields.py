bff3593fc779975ba2dd4a479987115c61e08b01
Florian Holderied <user.email>
2020-08-20 13:46:39 +0200

    1. changes order of particle array from (Np, 7) to (7, Np) to account for C-order. 2. merged field evaluation with accumulation and pusher routines

--- hylife/utilitis_PIC/fields.py
+++ hylife/utilitis_PIC/fields.py
@@ -27,44 +27,54 @@ def evaluate_1form(particles_pos, t1, t2, t3, p, nel, nbase_n, nbase_d, np, u1, 
     b2  = empty((p2 + 1, p2 + 1), dtype=float)
     b3  = empty((p3 + 1, p3 + 1), dtype=float)
     
+    l1  = empty( p1             , dtype=float)
+    l2  = empty( p2             , dtype=float)
+    l3  = empty( p3             , dtype=float)
+    
+    r1  = empty( p1             , dtype=float)
+    r2  = empty( p2             , dtype=float)
+    r3  = empty( p3             , dtype=float)
+    
     # scaling arrays for M-splines
     d1  = empty( p1             , dtype=float)
     d2  = empty( p2             , dtype=float)
     d3  = empty( p3             , dtype=float)
     
     #$ omp parallel
-    #$ omp do private(ip, pos1, pos2, pos3, span1, span2, span3, b1, b2, b3, d1, d2, d3)
+    #$ omp do private(ip, pos1, pos2, pos3, span1, span2, span3, b1, b2, b3, l1, l2, l3, r1, r2, r3, d1, d2, d3)
     for ip in range(np):
         
-        pos1 = particles_pos[ip, 0]
-        pos2 = particles_pos[ip, 1]
-        pos3 = particles_pos[ip, 2]
+        pos1 = particles_pos[0, ip]
+        pos2 = particles_pos[1, ip]
+        pos3 = particles_pos[2, ip]
         
         # evaluation of equilibrium field
-        u_part[ip, 0] = eq_mhd.u1_eq(pos1, pos2, pos3, kind_map, params_map)
-        u_part[ip, 1] = eq_mhd.u2_eq(pos1, pos2, pos3, kind_map, params_map)
-        u_part[ip, 2] = eq_mhd.u3_eq(pos1, pos2, pos3, kind_map, params_map)
+        u_part[0, ip] = eq_mhd.u1_eq(pos1, pos2, pos3, kind_map, params_map)
+        u_part[1, ip] = eq_mhd.u2_eq(pos1, pos2, pos3, kind_map, params_map)
+        u_part[2, ip] = eq_mhd.u3_eq(pos1, pos2, pos3, kind_map, params_map)
         
         # evaluation of perturbed field
         span1 = int(pos1*nel[0]) + p1
         span2 = int(pos2*nel[1]) + p2
         span3 = int(pos3*nel[2]) + p3
         
-        bsp.basis_funs_all(t1, p1, pos1, span1, b1, d1)
-        bsp.basis_funs_all(t2, p2, pos2, span2, b2, d2)
-        bsp.basis_funs_all(t3, p3, pos3, span3, b3, d3)
+        bsp.basis_funs_all(t1, p1, pos1, span1, l1, r1, b1, d1)
+        bsp.basis_funs_all(t2, p2, pos2, span2, l2, r2, b2, d2)
+        bsp.basis_funs_all(t3, p3, pos3, span3, l3, r3, b3, d3)
 
         # 1 - component (DNN)
-        u_part[ip, 0] += eva.evaluation_kernel(pd1, p2, p3, b1[pd1, :p1]*d1[:], b2[p2], b3[p3], span1 - 1, span2, span3, nbase_d[0], nbase_n[1], nbase_n[2], u1)
+        u_part[0, ip] += eva.evaluation_kernel(pd1, p2, p3, b1[pd1, :p1]*d1[:], b2[p2], b3[p3], span1 - 1, span2, span3, nbase_d[0], nbase_n[1], nbase_n[2], u1)
         
         # 2 - component (NDN)
-        u_part[ip, 1] += eva.evaluation_kernel(p1, pd2, p3, b1[p1], b2[pd2, :p2]*d2[:], b3[p3], span1, span2 - 1, span3, nbase_n[0], nbase_d[1], nbase_n[2], u2)
+        u_part[1, ip] += eva.evaluation_kernel(p1, pd2, p3, b1[p1], b2[pd2, :p2]*d2[:], b3[p3], span1, span2 - 1, span3, nbase_n[0], nbase_d[1], nbase_n[2], u2)
         
         # 3 - component (NND)
-        u_part[ip, 2] += eva.evaluation_kernel(p1, p2, pd3, b1[p1], b2[p2], b3[pd3, :p3]*d3[:], span1, span2, span3 - 1, nbase_n[0], nbase_n[1], nbase_d[2], u3)
+        u_part[2, ip] += eva.evaluation_kernel(p1, p2, pd3, b1[p1], b2[p2], b3[pd3, :p3]*d3[:], span1, span2, span3 - 1, nbase_n[0], nbase_n[1], nbase_d[2], u3)
     
     #$ omp end do
     #$ omp end parallel
+    
+    ierr = 0
     
     
 # ==========================================================================================================
@@ -82,9 +92,17 @@ def evaluate_2form(particles_pos, t1, t2, t3, p, nel, nbase_n, nbase_d, np, bb1,
     pd3 = p3 - 1
    
     # p + 1 non-vanishing basis functions up tp degree p
-    b1 = empty((p1 + 1, p1 + 1), dtype=float)
-    b2 = empty((p2 + 1, p2 + 1), dtype=float)
-    b3 = empty((p3 + 1, p3 + 1), dtype=float)
+    b1  = empty((p1 + 1, p1 + 1), dtype=float)
+    b2  = empty((p2 + 1, p2 + 1), dtype=float)
+    b3  = empty((p3 + 1, p3 + 1), dtype=float)
+    
+    l1  = empty( p1             , dtype=float)
+    l2  = empty( p2             , dtype=float)
+    l3  = empty( p3             , dtype=float)
+    
+    r1  = empty( p1             , dtype=float)
+    r2  = empty( p2             , dtype=float)
+    r3  = empty( p3             , dtype=float)
     
     # scaling arrays for M-splines
     d1 = empty( p1             , dtype=float)
@@ -92,35 +110,37 @@ def evaluate_2form(particles_pos, t1, t2, t3, p, nel, nbase_n, nbase_d, np, bb1,
     d3 = empty( p3             , dtype=float)
     
     #$ omp parallel
-    #$ omp do private(ip, pos1, pos2, pos3, span1, span2, span3, b1, b2, b3, d1, d2, d3)
+    #$ omp do private(ip, pos1, pos2, pos3, span1, span2, span3, b1, b2, b3, l1, l2, l3, r1, r2, r3, d1, d2, d3)
     for ip in range(np):
         
-        pos1 = particles_pos[ip, 0]
-        pos2 = particles_pos[ip, 1]
-        pos3 = particles_pos[ip, 2]
+        pos1 = particles_pos[0, ip]
+        pos2 = particles_pos[1, ip]
+        pos3 = particles_pos[2, ip]
         
         # evaluation of equilibrium field
-        b_part[ip, 0] = eq_mhd.b1_eq(pos1, pos2, pos3, kind_map, params_map)
-        b_part[ip, 1] = eq_mhd.b2_eq(pos1, pos2, pos3, kind_map, params_map)
-        b_part[ip, 2] = eq_mhd.b3_eq(pos1, pos2, pos3, kind_map, params_map)
+        b_part[0, ip] = eq_mhd.b1_eq(pos1, pos2, pos3, kind_map, params_map)
+        b_part[1, ip] = eq_mhd.b2_eq(pos1, pos2, pos3, kind_map, params_map)
+        b_part[2, ip] = eq_mhd.b3_eq(pos1, pos2, pos3, kind_map, params_map)
         
         # evaluation of perturbed field
         span1 = int(pos1*nel[0]) + p1
         span2 = int(pos2*nel[1]) + p2
         span3 = int(pos3*nel[2]) + p3
         
-        bsp.basis_funs_all(t1, p1, pos1, span1, b1, d1)
-        bsp.basis_funs_all(t2, p2, pos2, span2, b2, d2)
-        bsp.basis_funs_all(t3, p3, pos3, span3, b3, d3)
+        bsp.basis_funs_all(t1, p1, pos1, span1, l1, r1, b1, d1)
+        bsp.basis_funs_all(t2, p2, pos2, span2, l2, r2, b2, d2)
+        bsp.basis_funs_all(t3, p3, pos3, span3, l3, r3, b3, d3)
 
         # 1 - component (NDD)
-        b_part[ip, 0] += eva.evaluation_kernel(p1, pd2, pd3, b1[p1], b2[pd2, :p2]*d2[:], b3[pd3, :p3]*d3[:], span1, span2 - 1, span3 - 1, nbase_n[0], nbase_d[1], nbase_d[2], bb1)
+        b_part[0, ip] += eva.evaluation_kernel(p1, pd2, pd3, b1[p1], b2[pd2, :p2]*d2[:], b3[pd3, :p3]*d3[:], span1, span2 - 1, span3 - 1, nbase_n[0], nbase_d[1], nbase_d[2], bb1)
         
         # 2 - component (DND)
-        b_part[ip, 1] += eva.evaluation_kernel(pd1, p2, pd3, b1[pd1, :p1]*d1[:], b2[p2], b3[pd3, :p3]*d3[:], span1 - 1, span2, span3 - 1, nbase_d[0], nbase_n[1], nbase_d[2], bb2)
+        b_part[1, ip] += eva.evaluation_kernel(pd1, p2, pd3, b1[pd1, :p1]*d1[:], b2[p2], b3[pd3, :p3]*d3[:], span1 - 1, span2, span3 - 1, nbase_d[0], nbase_n[1], nbase_d[2], bb2)
         
         # 3 - component (DDN)
-        b_part[ip, 2] += eva.evaluation_kernel(pd1, pd2, p3, b1[pd1, :p1]*d1[:], b2[pd2, :p2]*d2[:], b3[p3], span1 - 1, span2 - 1, span3, nbase_d[0], nbase_d[1], nbase_n[2], bb3)
+        b_part[2, ip] += eva.evaluation_kernel(pd1, pd2, p3, b1[pd1, :p1]*d1[:], b2[pd2, :p2]*d2[:], b3[p3], span1 - 1, span2 - 1, span3, nbase_d[0], nbase_d[1], nbase_n[2], bb3)
     
     #$ omp end do
     #$ omp end parallel
+    
+    ierr = 0
