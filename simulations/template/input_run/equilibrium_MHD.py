911a9f2cb39ed8d5ec367c518e4880cd3a734c83
Florian Holderied <user.email>
2021-05-11 17:05:03 +0200

    added equilibrium current to input file

--- simulations/template/input_run/equilibrium_MHD.py
+++ simulations/template/input_run/equilibrium_MHD.py
@@ -16,30 +16,6 @@ def p_eq(x, y, z):
     
     return p
 
-# equilibrium bulk velocity (x - component)
-@types('double','double','double')
-def u_eq_x(x, y, z):
-    
-    ux = 0.
-    
-    return ux
-
-# equilibrium bulk velocity (y - component)
-@types('double','double','double')
-def u_eq_y(x, y, z):
-    
-    uy = 0.
-    
-    return uy
-
-# equilibrium bulk velocity (z - component)
-@types('double','double','double')
-def u_eq_z(x, y, z):
-    
-    uz = 0.
-    
-    return uz
-
 # equilibrium magnetic field (x - component)
 @types('double','double','double')
 def b_eq_x(x, y, z):
@@ -63,6 +39,30 @@ def b_eq_z(x, y, z):
     bz = 0.
     
     return bz
+
+# equilibrium current (x - component, curl of equilibrium magnetic field)
+@types('double','double','double')
+def j_eq_x(x, y, z):
+    
+    jx = 0.
+    
+    return jx
+
+# equilibrium current (y - component, curl of equilibrium magnetic field)
+@types('double','double','double')
+def j_eq_y(x, y, z):
+    
+    jy = 0.
+    
+    return jy
+
+# equilibrium current (z - component, curl of equilibrium magnetic field)
+@types('double','double','double')
+def j_eq_z(x, y, z):
+    
+    jz = 0.
+    
+    return jz
 
 # equilibrium bulk density
 @types('double','double','double')
@@ -159,3 +159,46 @@ def b2_eq_3(eta1, eta2, eta3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, 
     bz = b_eq_z(x, y, z)
     
     return pull.pull_2_form(bx, by, bz, eta1, eta2, eta3, 3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+
+
+# equilibrium current (2-form on logical domain, 1-component)
+@types('double','double','double','int','double[:]','double[:]','double[:]','double[:]','int[:]','int[:]','double[:,:,:]','double[:,:,:]','double[:,:,:]')
+def j2_eq_1(eta1, eta2, eta3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz):
+    
+    x = mapping.f(eta1, eta2, eta3, 1, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    y = mapping.f(eta1, eta2, eta3, 2, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    z = mapping.f(eta1, eta2, eta3, 3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    
+    jx = j_eq_x(x, y, z)
+    jy = j_eq_y(x, y, z)
+    jz = j_eq_z(x, y, z)
+    
+    return pull.pull_2_form(jx, jy, jz, eta1, eta2, eta3, 1, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+
+# equilibrium current (2-form on logical domain, 2-component)
+@types('double','double','double','int','double[:]','double[:]','double[:]','double[:]','int[:]','int[:]','double[:,:,:]','double[:,:,:]','double[:,:,:]')
+def j2_eq_2(eta1, eta2, eta3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz):
+    
+    x = mapping.f(eta1, eta2, eta3, 1, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    y = mapping.f(eta1, eta2, eta3, 2, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    z = mapping.f(eta1, eta2, eta3, 3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    
+    jx = j_eq_x(x, y, z)
+    jy = j_eq_y(x, y, z)
+    jz = j_eq_z(x, y, z)
+    
+    return pull.pull_2_form(jx, jy, jz, eta1, eta2, eta3, 2, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+
+# equilibrium current (2-form on logical domain, 3-component)
+@types('double','double','double','int','double[:]','double[:]','double[:]','double[:]','int[:]','int[:]','double[:,:,:]','double[:,:,:]','double[:,:,:]')
+def j2_eq_3(eta1, eta2, eta3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz):
+    
+    x = mapping.f(eta1, eta2, eta3, 1, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    y = mapping.f(eta1, eta2, eta3, 2, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    z = mapping.f(eta1, eta2, eta3, 3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
+    
+    jx = j_eq_x(x, y, z)
+    jy = j_eq_y(x, y, z)
+    jz = j_eq_z(x, y, z)
+    
+    return pull.pull_2_form(jx, jy, jz, eta1, eta2, eta3, 3, kind_map, params_map, tn1, tn2, tn3, pn, nbase_n, cx, cy, cz)
