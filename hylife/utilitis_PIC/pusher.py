8d02ed548df63a716c436bfcee59bc390fcbb9c1
Florian Holderied <user.email>
2021-06-01 10:45:38 +0200

    added analytical solution to sub-step 5 (v x B rotation)

--- hylife/utilitis_PIC/pusher.py
+++ hylife/utilitis_PIC/pusher.py
@@ -549,3 +549,157 @@ def pusher_step5(particles, dt, t1, t2, t3, p, nel, nbase_n, nbase_d, np, bb1, b
     #$ omp end parallel
     
     ierr = 0
+    
+    
+    
+    
+# ==========================================================================================================
+@types('double[:,:]','double','double[:]','double[:]','double[:]','int[:]','int[:]','int[:]','int[:]','int','double[:,:,:]','double[:,:,:]','double[:,:,:]','int','double[:]','double[:]','double[:]','double[:]','int[:]','int[:]','int[:]','double[:,:,:]','double[:,:,:]','double[:,:,:]')
+def pusher_step5_ana(particles, dt, t1, t2, t3, p, nel, nbase_n, nbase_d, np, bb1, bb2, bb3, kind_map, params_map, tf1, tf2, tf3, pf, nelf, nbasef, cx, cy, cz):
+    
+    from numpy import empty, zeros, sqrt, cos, sin
+    
+    # ============== for magnetic field evaluation ============
+    # spline degrees
+    pn1 = p[0]
+    pn2 = p[1]
+    pn3 = p[2]
+    
+    pd1 = pn1 - 1
+    pd2 = pn2 - 1
+    pd3 = pn3 - 1
+    
+    # p + 1 non-vanishing basis functions up tp degree p
+    b1  = empty((pn1 + 1, pn1 + 1), dtype=float)
+    b2  = empty((pn2 + 1, pn2 + 1), dtype=float)
+    b3  = empty((pn3 + 1, pn3 + 1), dtype=float)
+    
+    # left and right values for spline evaluation
+    l1  = empty( pn1, dtype=float)
+    l2  = empty( pn2, dtype=float)
+    l3  = empty( pn3, dtype=float)
+    
+    r1  = empty( pn1, dtype=float)
+    r2  = empty( pn2, dtype=float)
+    r3  = empty( pn3, dtype=float)
+    
+    # scaling arrays for M-splines
+    d1  = empty( pn1, dtype=float)
+    d2  = empty( pn2, dtype=float)
+    d3  = empty( pn3, dtype=float)
+    
+    # magnetic field at particle position (2-form, cartesian, normalized cartesian)
+    b      = empty(3, dtype=float)
+    b_cart = empty(3, dtype=float)
+    b0     = empty(3, dtype=float)
+    
+    # particle velocity (cartesian, perpendicular, v x b0, b0 x vperp)
+    v        = empty(3, dtype=float)
+    vperp    = empty(3, dtype=float)
+    vxb0     = empty(3, dtype=float)
+    b0xvperp = empty(3, dtype=float)
+    # ==========================================================
+    
+    
+    # ================ for mapping evaluation ==================
+    # spline degrees
+    pf1   = pf[0]
+    pf2   = pf[1]
+    pf3   = pf[2]
+    
+    # pf + 1 non-vanishing basis functions up tp degree pf
+    b1f   = empty((pf1 + 1, pf1 + 1), dtype=float)
+    b2f   = empty((pf2 + 1, pf2 + 1), dtype=float)
+    b3f   = empty((pf3 + 1, pf3 + 1), dtype=float)
+    
+    # left and right values for spline evaluation
+    l1f   = empty( pf1, dtype=float)
+    l2f   = empty( pf2, dtype=float)
+    l3f   = empty( pf3, dtype=float)
+    
+    r1f   = empty( pf1, dtype=float)
+    r2f   = empty( pf2, dtype=float)
+    r3f   = empty( pf3, dtype=float)
+    
+    # scaling arrays for M-splines
+    d1f   = empty( pf1, dtype=float)
+    d2f   = empty( pf2, dtype=float)
+    d3f   = empty( pf3, dtype=float)
+    
+    # pf + 1 derivatives
+    der1f = empty( pf1 + 1, dtype=float)
+    der2f = empty( pf2 + 1, dtype=float)
+    der3f = empty( pf3 + 1, dtype=float)
+    
+    # needed mapping quantities
+    df    = empty((3, 3), dtype=float)
+    # ==========================================================
+    
+    
+    #$ omp parallel
+    #$ omp do private (ip, eta1, eta2, eta3, span1, span2, span3, l1, l2, l3, r1, r2, r3, b1, b2, b3, d1, d2, d3, b, span1f, span2f, span3f, l1f, l2f, l3f, r1f, r2f, r3f, b1f, b2f, b3f, d1f, d2f, d3f, der1f, der2f, der3f, df, det_df, v, b_cart, b_norm, b0, vpar, vxb0, vperp, b0xvperp)
+    for ip in range(np):
+        
+        eta1 = particles[0, ip]
+        eta2 = particles[1, ip]
+        eta3 = particles[2, ip]
+        
+        # ========== field evaluation ==============
+        span1 = int(eta1*nel[0]) + pn1
+        span2 = int(eta2*nel[1]) + pn2
+        span3 = int(eta3*nel[2]) + pn3
+        
+        # evaluation of basis functions
+        bsp.basis_funs_all(t1, pn1, eta1, span1, l1, r1, b1, d1)
+        bsp.basis_funs_all(t2, pn2, eta2, span2, l2, r2, b2, d2)
+        bsp.basis_funs_all(t3, pn3, eta3, span3, l3, r3, b3, d3)
+        
+        # magnetic field (2-form)
+        b[0] = eva.evaluation_kernel(pn1, pd2, pd3, b1[pn1], b2[pd2, :pn2]*d2[:], b3[pd3, :pn3]*d3[:], span1, span2 - 1, span3 - 1, nbase_n[0], nbase_d[1], nbase_d[2], bb1)
+        b[1] = eva.evaluation_kernel(pd1, pn2, pd3, b1[pd1, :pn1]*d1[:], b2[pn2], b3[pd3, :pn3]*d3[:], span1 - 1, span2, span3 - 1, nbase_d[0], nbase_n[1], nbase_d[2], bb2)
+        b[2] = eva.evaluation_kernel(pd1, pd2, pn3, b1[pd1, :pn1]*d1[:], b2[pd2, :pn2]*d2[:], b3[pn3], span1 - 1, span2 - 1, span3, nbase_d[0], nbase_d[1], nbase_n[2], bb3)
+        # ==========================================
+        
+        
+        # ========= mapping evaluation =============
+        span1f = int(eta1*nelf[0]) + pf1
+        span2f = int(eta2*nelf[1]) + pf2
+        span3f = int(eta3*nelf[2]) + pf3
+        
+        # evaluate Jacobian matrix
+        mapping_fast.df_all(kind_map, params_map, tf1, tf2, tf3, pf, nbasef, span1f, span2f, span3f, cx, cy, cz, l1f, l2f, l3f, r1f, r2f, r3f, b1f, b2f, b3f, d1f, d2f, d3f, der1f, der2f, der3f, eta1, eta2, eta3, df)
+        
+        # evaluate Jacobian determinant
+        det_df = abs(linalg.det(df))
+        # ==========================================
+        
+        
+        # ======== particle pushing ================
+        v[:] = particles[3:6, ip]
+        
+        # push-forward of magnetic field
+        linalg.matrix_vector(df/det_df, b, b_cart)
+        
+        # absolute value of magnetic field
+        b_norm = sqrt(b_cart[0]**2 + b_cart[1]**2 + b_cart[2]**2)
+        
+        # normalized magnetic field direction
+        b0[:] = b_cart/b_norm
+        
+        # parallel velocity v * b0
+        vpar = v[0]*b0[0] + v[1]*b0[1] + v[2]*b0[2]
+        
+        # perpendicular velocity b0 x (v x b0)
+        linalg.cross(v, b0, vxb0)
+        linalg.cross(b0, vxb0, vperp)
+        
+        # analytical rotation
+        linalg.cross(b0, vperp, b0xvperp)
+        
+        particles[3:6, ip] = vpar*b0 + cos(b_norm*dt)*vperp - sin(b_norm*dt)*b0xvperp
+        # ==========================================
+    
+    #$ omp end do
+    #$ omp end parallel
+    
+    ierr = 0
