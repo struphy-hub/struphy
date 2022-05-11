c31a7c9eb58a305fc430cb32a5d9ab27ac473884
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2022-05-11 15:16:31 +0000

    Fix issue 27

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="struphy",
-    version="1.7",
+    version="1.8.2",
     packages=setuptools.find_packages(),
     package_data={
         'struphy.mhd_equil': [
@@ -13,22 +13,7 @@ setup(
             'gvec/*.hdf5',
         ],
         'struphy.io': ['batch/*.sh'],
-        'struphy.io.inp': [
-            'lin_mhd/*.yml',
-            'lin_mhd_MF/*.yml',
-            'lin_mhd_psydac/*.yml',
-            'cc_lin_mhd_6d/*.yml',
-            'cc_lin_mhd_6d_MF/*.yml',
-            'pc_lin_mhd_6d_MF_full/*.yml',
-            'pc_lin_mhd_6d_MF_perp/*.yml',
-            'kinetic_extended/*.yml',
-            'lin_Vlasov_Maxwell/*yml',
-            'maxwell/*yml',
-            'maxwell_psydac/*yml',
-            'cold_plasma/*yml',
-            'cc_cold_plasma_6d/*yml',
-            'inverse_mass_test/*yml',
-        ],
+        'struphy.io.inp': ['parameters.yml'],
         'struphy': ['compile_struphy.mk'],
     },
     # list of executable(s) that come with the package (if applicable)s
@@ -63,10 +48,10 @@ setup(
         'wheel',
     ],
     # more information, necessary for an upload to PyPI
-    author="Stefan Possanner",
-    author_email="spossann@ipp.mpg.de",
-    description="Multi-model plasma physics package for the simulation energetic particles",
+    author="Max Planck Institute for Plasma Physics, Garching, Germany",
+    author_email="stefan.possanner@ipp.mpg.de, florian.holderied@ipp.mpg.de, xin.wang@ipp.mpg.de",
+    description="Multi-model plasma physics package",
     license="GNU",
-    keywords="plasma, energetic particles, particle-in-cell, discrete differential forms",
+    keywords="plasma, partial differential equations, energetic particles",
     url="https://clapp.pages.mpcdf.de/hylife/",   # project home page, if any
 )
