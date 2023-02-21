Fixing errors at the Implicit discrete gradient particle pusher

struphy/pic/pusher.py
@@ -74,8 +74,7 @@ class Pusher:
                           9:12] = particles.markers[~particles.holes, 0:3]
 
         if particles.kinds == 'Particles5D':
-            particles.markers[~particles.holes,
-                              12] = particles.markers[~particles.holes, 3]
+            particles.markers[~particles.holes, 12] = particles.markers[~particles.holes, 3]
 
         for stage in range(self._n_stages):
             self._pusher(particles.markers, dt, stage, *
@@ -84,7 +83,7 @@ class Pusher:
             # apply boundary conditions to markers
             if bc is not None:
                 apply_kinetic_bc(particles.markers,
-                                 particles.holes, self.domain, bc)
+                                 particles.holes, self.domain, bc, self._derham.comm)
 
             # sort markers according to domain decomposition
             if mpi_sort == 'each':
@@ -228,7 +227,6 @@ class Pusher_iteration:
         self._maxiter = maxiter
         self._tol = tol
 
-        # get FEM information
         self._args_fem = (derham.domain_array[derham.comm.Get_rank(), :],
                           np.array(derham.p),
                           derham.Vh_fem['0'].knots[0], derham.Vh_fem['0'].knots[1], derham.Vh_fem['0'].knots[2],
@@ -240,10 +238,9 @@ class Pusher_iteration:
         # select kernels
         self._pusher_name = pusher_name
         self._pusher = getattr(pushers, self._pusher_name)
-        self._eval_S_and_I = getattr(
-            utilities, self._pusher_name + '_eval_S_and_I')
-        self._initial_guess = getattr(
-            utilities, self._pusher_name + '_initial_guess')
+        self._pusher_prepare = getattr(utilities, self._pusher_name + '_prepare')
+        self._pusher_eval_gradI = getattr(utilities, self._pusher_name + '_eval_gradI')
+
 
     def __call__(self, particles, dt, *args_opt, bc=None, mpi_sort=None, verbose=False):
         """
@@ -275,45 +272,46 @@ class Pusher_iteration:
         # TODO: only applicable to Particle5D case! if we have any iterative solver with Particle6D then we should generalize
         # TODO: maybe we can modulize ... discrete gradient method, fixed-point iterative solver ...
 
+        assert bc is not None
+
         # save initial etas in columns 9:12
-        particles.markers[~particles.holes,
-                          9:13] = particles.markers[~particles.holes, 0:4]
+        particles.markers[~particles.holes, 9:13] = particles.markers[~particles.holes, 0:4]
 
-        # before iteration:
-        # TODO: we can unify eval_S_I and initial_guess
-        # save S(z_n) at 13:19 and I(z_n) at 19
-        self._eval_S_and_I(particles.markers, *self.args_fem,
-                           *self.domain.args_map, *args_opt)
+        # prepare the iteration:
+        self._pusher_prepare(particles.markers, dt, *self.args_fem, *self.domain.args_map, *args_opt)
+        
+        # sorting
+        apply_kinetic_bc(particles.markers, particles.holes, self.domain, bc, self._derham.comm)
+        particles.mpi_sort_markers()
 
-        # save initial guess in columns 0:3
-        self._initial_guess(particles.markers, dt, *
-                            self.args_fem, *self.domain.args_map, *args_opt)
+        # eval gradI 
+        self._pusher_eval_gradI(particles.markers, dt, *self.args_fem, *self.domain.args_map, *args_opt)
+        
+        # sorting
+        apply_kinetic_bc(particles.markers, particles.holes, self.domain, bc, self._derham.comm)
+        particles.mpi_sort_markers()
 
         # start iteration
         for stage in range(self._maxiter):
 
-            self._pusher(particles.markers, dt, stage, self._tol,
-                         *self.args_fem, *self.domain.args_map, *args_opt)
+            self._pusher(particles.markers, dt, stage, self._tol, *self.args_fem, *self.domain.args_map, *args_opt)
 
-            # apply boundary conditions to markers
-            if bc is not None:
-                apply_kinetic_bc(particles.markers,
-                                 particles.holes, self.domain, bc)
+            # sorting
+            apply_kinetic_bc(particles.markers, particles.holes, self.domain, bc, self._derham.comm)
+            particles.mpi_sort_markers()
 
-            # sort markers according to domain decomposition
-            if mpi_sort == 'each':
-                particles.mpi_sort_markers()
+            self._pusher_eval_gradI(particles.markers, dt, *self.args_fem, *self.domain.args_map, *args_opt)
+
+            # sorting
+            apply_kinetic_bc(particles.markers, particles.holes, self.domain, bc, self._derham.comm)
+            particles.mpi_sort_markers()
 
             # print stage info
             if self._derham.comm.Get_rank() == 0 and verbose:
                 print(self._pusher_name, 'done. (stage :', stage + 1, ')')
 
-        # sort markers according to domain decomposition
-        if mpi_sort == 'last':
-            particles.mpi_sort_markers()
-
         # clear buffer columns 9-23 for multi-stage pushers
-        particles.markers[~particles.holes, 9:23] = 0.
+        particles.markers[~particles.holes, 9:24] = 0.
 
     @property
     def derham(self):