Fix pesky sign error, energy is now conserved

src/struphy/models/kinetic.py
@@ -1307,23 +1307,23 @@ class DeltaFVlasovAmpereOneSpecies(StruphyModel):
         algo_eta = params['kinetic']['species1']['options']['algos']['push_eta']
         params_coupling = params['kinetic']['species1']['options']['coupling_solver']
 
-        # # Initialize propagators/integrators used in splitting substeps
-        # self.add_propagator(self.prop_markers.PushEta(
-        #     self.pointer['species1'],
-        #     algo=algo_eta,
-        #     bc_type=self._species_params['markers']['bc']['type']))
-        # if self._rank == 0:
-        #     print("Added Step PushEta\n")
+        # Initialize propagators/integrators used in splitting substeps
+        self.add_propagator(self.prop_markers.PushEta(
+            self.pointer['species1'],
+            algo=algo_eta,
+            bc_type=self._species_params['markers']['bc']['type']))
+        if self._rank == 0:
+            print("Added Step PushEta\n")
 
-        # self.add_propagator(self.prop_coupling.EfieldWeightsAnalytic(
-        #     self.pointer['e_field'],
-        #     self.pointer['species1'],
-        #     alpha=self.alpha,
-        #     kappa=self.kappa,
-        #     f0=self._f0,
-        #     **params_coupling))
-        # if self._rank == 0:
-        #     print("\nAdded Step EfieldWeights Analytic\n")
+        self.add_propagator(self.prop_coupling.EfieldWeightsAnalytic(
+            self.pointer['e_field'],
+            self.pointer['species1'],
+            alpha=self.alpha,
+            kappa=self.kappa,
+            f0=self._f0,
+            **params_coupling))
+        if self._rank == 0:
+            print("\nAdded Step EfieldWeights Analytic\n")
 
         self.add_propagator(self.prop_coupling.EfieldWeightsDiscreteGradient(
             self.pointer['e_field'],
@@ -1335,12 +1335,12 @@ class DeltaFVlasovAmpereOneSpecies(StruphyModel):
         if self._rank == 0:
             print("\nAdded Step EfieldWeights Discrete Gradient\n")
 
-        # self.add_propagator(self.prop_markers.StepWeightsVelocities(
-        #     self.pointer['species1'],
-        #     e_field=(self._e_background + self.pointer['e_field']),
-        #     kappa=self.kappa,
-        #     f0=self._f0,
-        # ))
+        self.add_propagator(self.prop_markers.StepWeightsVelocities(
+            self.pointer['species1'],
+            e_field=(self._e_background + self.pointer['e_field']),
+            kappa=self.kappa,
+            f0=self._f0,
+        ))
 
         # Scalar variables to be saved during simulation
         self.add_scalar('en_e')
@@ -1402,7 +1402,7 @@ class DeltaFVlasovAmpereOneSpecies(StruphyModel):
 
         # - alpha^2 * v_th^2 / N * sum_p w_p * ln(f0_p)
         self._tmp[0] = \
-            self.alpha**2 * self.vth**2 / self.pointer['species1'].n_mks * \
+            - self.alpha**2 * self.vth**2 / self.pointer['species1'].n_mks * \
             np.dot(
                 self.pointer['species1'].markers_wo_holes[:, 6],
                 ln_f0_values

src/struphy/propagators/propagators_coupling.py
@@ -702,12 +702,7 @@ class EfieldWeightsDiscreteGradient(Propagator):
     def __call__(self, dt):
         # evaluate f0 and accumulate
         f0_values = self._f0(
-            self.particles[0].markers[:, 0],
-            self.particles[0].markers[:, 1],
-            self.particles[0].markers[:, 2],
-            self.particles[0].markers[:, 3],
-            self.particles[0].markers[:, 4],
-            self.particles[0].markers[:, 5]
+            *self.particles[0].markers[:, :6].T
         )
 
         self._accum.accumulate(
@@ -734,7 +729,7 @@ class EfieldWeightsDiscreteGradient(Propagator):
         self._e_weights *= 0.
         self._e_weights += self.feec_vars[0]
         self._e_weights += self._e_tmp
-        self._e_weights *= 0.5
+        self._e_weights /= 2.
 
         # Update weights
         self._pusher(
@@ -747,13 +742,13 @@ class EfieldWeightsDiscreteGradient(Propagator):
         )
 
         # write new coeffs into self.variables
-        max_de, = self.feec_vars_update(self._e_tmp)
+        max_de = self.feec_vars_update(self._e_tmp)
 
         # Print out max differences for weights and e-field
         if self._info:
             print('Status          for StepEfieldWeights:', info['success'])
             print('Iterations      for StepEfieldWeights:', info['niter'])
-            print('Maxdiff    e1   for StepEfieldWeights:', max_de)
+            print('Maxdiff    e1   for StepEfieldWeights:', max_de[0])
             max_diff = np.max(np.abs(self._old_weights[~self.particles[0].holes]
                                      - self.particles[0].markers[~self.particles[0].holes, 6]))
             print('Maxdiff weights for StepEfieldWeights:', max_diff)
@@ -866,12 +861,7 @@ class EfieldWeightsAnalytic(Propagator):
     def __call__(self, dt):
         # evaluate f0 and accumulate
         f0_values = self._f0(
-            self.particles[0].markers[:, 0],
-            self.particles[0].markers[:, 1],
-            self.particles[0].markers[:, 2],
-            self.particles[0].markers[:, 3],
-            self.particles[0].markers[:, 4],
-            self.particles[0].markers[:, 5]
+            *self.particles[0].markers[:, :6].T
         )
 
         self._accum.accumulate(
@@ -895,9 +885,9 @@ class EfieldWeightsAnalytic(Propagator):
 
         # Compute vector for particle pushing
         self._e_weights *= 0.
-        self._e_weights += self._delta_e
+        self._e_weights -= self._delta_e
         self._e_weights *= 0.5
-        self._e_weights -= self._e_tmp
+        self._e_weights += self._e_tmp
 
         # Update weights
         self._pusher(