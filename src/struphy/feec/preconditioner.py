b9c507197e22f766af210b43850f119100dc9d4b
Max <max.lindqvist@ipp.mpg.de>
2024-10-04 10:04:16 +0200

    Corrected arguments for KroneckerLinearSolver.

--- src/struphy/feec/preconditioner.py
+++ src/struphy/feec/preconditioner.py
@@ -511,12 +511,12 @@ class MassMatrixDiagonalPreconditioner(LinearOperator):
                 matrixblocks += [KroneckerStencilMatrix(
                     self._femspace.vector_space, self._femspace.vector_space, *matrixcells)]
                 solverblocks += [KroneckerLinearSolver(
-                    self._femspace.vector_space, solvercells)]
+                    self._femspace.vector_space, self._femspace.vector_space, solvercells)]
             else:
                 matrixblocks += [KroneckerStencilMatrix(
                     self._femspace.vector_space[c], self._femspace.vector_space[c], *matrixcells)]
                 solverblocks += [KroneckerLinearSolver(
-                    self._femspace.vector_space[c], solvercells)]
+                    self._femspace.vector_space[c], self._femspace.vector_space[c], solvercells)]
 
         # build final matrix and solver
         if isinstance(self._femspace, TensorFemSpace):
