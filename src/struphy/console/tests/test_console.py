9d59b8bce6cff92e21fc59daf57b6cc4128881c9
Max <max.lindqvist@ipp.mpg.de>
2024-10-14 16:58:15 +0200

    Simplify pytest so that the returned subprocess command is checked instead of writing and reading a batch_script.sh file.

--- src/struphy/console/tests/test_console.py
+++ src/struphy/console/tests/test_console.py
@@ -12,7 +12,8 @@ def is_sublist(main_list, sub_list):
     Check if sub_list is a sublist of main_list.
     """
     sub_len = len(sub_list)
-    return any(main_list[i:i + sub_len] == sub_list for i in range(len(main_list) - sub_len + 1))
+    return any(main_list[i:i + sub_len] ==
+               sub_list for i in range(len(main_list) - sub_len + 1))
 
 
 def split_command(command):
@@ -29,8 +30,8 @@ def split_command(command):
 
 @pytest.mark.parametrize('model', ['Maxwell', 'Vlasov'])
 @pytest.mark.parametrize('input_abs', [os.path.join(libpath, 'io/inp/parameters.yml')])
-@pytest.mark.parametrize('output_abs', [os.path.join(libpath, 'io/out/')])
-@pytest.mark.parametrize('batch_abs', [None, os.path.join(libpath, 'io/batch/batch_cobra.sh')])
+@pytest.mark.parametrize('output_abs', [os.path.join(libpath, 'io/out/sim_1')])
+@pytest.mark.parametrize('batch_abs',[None, os.path.join(libpath, 'io/batch/batch_cobra.sh')])
 @pytest.mark.parametrize('restart', [False, True])
 @pytest.mark.parametrize('cprofile', [False, True])
 @pytest.mark.parametrize('likwid', [False, True])
@@ -38,57 +39,57 @@ def split_command(command):
 @pytest.mark.parametrize('save_step', [1, 300])
 @pytest.mark.parametrize('mpi', [1, 2])
 @patch('subprocess.run')
-def test_struphy_run(mock_run, model, input_abs, output_abs, batch_abs, runtime, save_step, restart, cprofile, likwid, mpi):
+def test_struphy_run(
+        mock_run,
+        model,
+        input_abs,
+        output_abs,
+        batch_abs,
+        runtime,
+        save_step,
+        restart,
+        cprofile,
+        likwid,
+        mpi):
 
-    struphy_run(model, input_abs=input_abs, output_abs=output_abs, batch_abs=batch_abs,
-                runtime=runtime, save_step=save_step, restart=restart, cprofile=cprofile, likwid=likwid, mpi=mpi)
+    run_command = struphy_run(
+        model,
+        input_abs=input_abs,
+        output_abs=output_abs,
+        batch_abs=batch_abs,
+        runtime=runtime,
+        save_step=save_step,
+        restart=restart,
+        cprofile=cprofile,
+        likwid=likwid,
+        mpi=mpi)
 
     mock_run.assert_called_once()
-    command = mock_run.call_args[0][0]
+    subprocess_call = mock_run.call_args[0][0]
 
     if batch_abs is not None:
-        assert command == ['sbatch', 'batch_script.sh']
-
-        batch_script_path = os.path.join(output_abs, 'batch_script.sh')
-
-        # Check that the batch script exists
-        assert os.path.exists(
-            batch_script_path), f"{batch_script_path} should exist"
-
-        # Read the content of the batch script
-        with open(batch_script_path, 'r') as f:
-            lines = f.readlines()
-
-        batch_run_command = lines[-1].strip().split(' ')
-
-        # Remove batch script
-        # os.remove(batch_script_path)
-        # assert not os.path.exists(batch_script_path), f"{batch_script_path} should have been deleted"
-
-        # Update the command to the command fron the batch script
-        command = batch_run_command
+        assert subprocess_call == ['sbatch', 'batch_script.sh']
 
         # This is only true if likwid == False, but is taken care of below
         mpirun_command = ['srun', 'python3']
         main = os.path.join(libpath, 'main.py')
-
-        assert '>' in command
     else:
         mpirun_command = ['mpirun', '-n', str(mpi), 'python3']
         main = 'main.py'
 
-    command = split_command(command)
+    run_command = split_command(run_command)
 
-    assert is_sublist(command, ['--runtime', str(runtime)])
-    assert is_sublist(command, ['-s', str(save_step)])
+    assert is_sublist(run_command, ['--runtime', str(runtime)])
+    assert is_sublist(run_command, ['-s', str(save_step)])
     if likwid:
         assert is_sublist(
-            command, ['likwid-mpirun', '-n', str(mpi), '-g', 'MEM_DP', '-stats'])
-        assert os.path.join(libpath, 'main.py') in command
+            run_command, [
+                'likwid-mpirun', '-n', str(mpi), '-g', 'MEM_DP', '-stats'])
+        assert os.path.join(libpath, 'main.py') in run_command
     else:
-        assert is_sublist(command, mpirun_command)
-        assert is_sublist(command, [main, model])
+        assert is_sublist(run_command, mpirun_command)
+        assert is_sublist(run_command, [main, model])
     if restart:
-        assert is_sublist(command, ['-r'])
+        assert is_sublist(run_command, ['-r'])
     if cprofile:
-        assert is_sublist(command, ['python3', '-m', 'cProfile'])
+        assert is_sublist(run_command, ['python3', '-m', 'cProfile'])
