8585b1a76d740076cbb2ae32c32031e06f3ee9f1
Max <max.lindqvist@ipp.mpg.de>
2024-10-14 17:19:28 +0200

    Refactored struphy_run

--- src/struphy/console/run.py
+++ src/struphy/console/run.py
@@ -1,20 +1,256 @@
-def struphy_run(model,
-                inp=None,
-                input_abs=None,
-                output='sim_1',
-                output_abs=None,
-                batch=None,
-                batch_abs=None,
-                runtime=300,
-                save_step=1,
-                restart=False,
-                mpi=1,
-                debug=False,
-                cprofile=False,
-                likwid=False,
-                likwid_inp=None,
-                likwid_input_abs=None,
-                likwid_repetitions=1,):
+import subprocess
+import shutil
+import os
+import struphy
+import yaml
+
+
+def update_kwargs_inplace(**kwargs):
+    """Update keyword arguments in-place with defaults."""
+    defaults = {
+        'inp': None,
+        'input_abs': None,
+        'output': 'sim_1',
+        'output_abs': None,
+        'batch': None,
+        'batch_abs': None,
+        'runtime': 300,
+        'save_step': 1,
+        'restart': False,
+        'mpi': 1,
+        'debug': False,
+        'cprofile': False,
+        'likwid': False,
+        'likwid_inp': None,
+        'likwid_input_abs': None,
+        'likwid_repetitions': 1
+    }
+    for key, default in defaults.items():
+        kwargs[key] = kwargs.get(key, default)
+    return kwargs
+
+
+def load_state(libpath):
+    """Load the Struphy state from a YAML file."""
+    state_path = os.path.join(libpath, 'state.yml')
+    with open(state_path) as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
+
+
+def generate_io_path(model, libpath, **kwargs):
+    
+    inp = kwargs.get('inp', None)
+    input_abs = kwargs.get('input_abs', None)
+    output = kwargs.get('output', 'sim_1')
+    output_abs = kwargs.get('output_abs', None)
+    batch = kwargs.get('batch', None)
+    batch_abs = kwargs.get('batch_abs', None)
+    restart = kwargs.get('restart', False)
+
+    # Struphy paths
+    state = load_state(libpath)
+    i_path = state['i_path']
+    o_path = state['o_path']
+    b_path = state['b_path']
+
+    if input_abs is None:
+        input_abs = _get_default_input_path(i_path, model, kwargs['inp'])
+        
+    if output_abs is None:
+        output_abs = os.path.join(o_path, output)
+
+    if batch_abs is None:
+        if batch is not None:
+            batch_abs = os.path.join(b_path, batch)
+
+    # take existing parameter file for restart
+    if restart:
+        input_abs = os.path.join(output_abs, 'parameters.yml')
+    
+    return input_abs, output_abs, batch_abs
+
+def _get_default_input_path(i_path, model, inp):
+    """Determine the default input path for a given model."""
+
+    if inp:
+        return os.path.join(i_path, inp)
+
+    default_yml = os.path.join(i_path, f'params_{model}.yml')
+    if os.path.isfile(default_yml):
+        print('\nRunning with default parameter file...')
+        return default_yml
+
+    from struphy.models import fluid, kinetic, hybrid, toy
+    model_classes = [fluid, kinetic, hybrid, toy]
+    for obj in model_classes:
+        try:
+            model_class = getattr(obj, model)
+            model_class.generate_default_parameter_file()
+        except AttributeError:
+            pass
+
+    exit()  # Exit if no suitable model class is found
+
+def generate_likwid_command(mpi, i_path, likwid_inp, likwid_input_abs):
+    
+    if not likwid_inp and not likwid_input_abs:
+        return ['likwid-mpirun', '-n', str(mpi), '-g', 'MEM_DP', '-stats', '-marker']
+
+    
+    if likwid_inp is not None:
+        likwid_input_abs = os.path.join(i_path, likwid_inp)
+    
+    with open(likwid_input_abs, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    # Get the command from the configuration
+    command_base = config.get('command', None)
+    if not command_base:
+        print("Missing required configuration: 'command'")
+        exit(1)
+    
+    likwid_config = config.get(command_base, {})
+    
+    # Get the options list
+    options = likwid_config.get('options', [])
+    
+    # Flatten the options list
+    flattened_options = ['-np',str(mpi)]
+    for item in options:
+        if isinstance(item, dict):
+            for key, value in item.items():
+                flattened_options.append(key)
+                flattened_options.append(str(value))  # Ensure the value is a string
+        else:
+            flattened_options.append(item)
+
+    
+    # Construct the command as a list
+    likwid_command = [command_base]
+    likwid_command.extend(flattened_options)
+    return likwid_command
+
+def cleanup_out_dir(output_abs):
+    """Clean up output directory by removing specific files."""
+    _ensure_dir_exists(output_abs)
+    _ensure_dir_exists(os.path.join(output_abs, 'data/'))
+    _remove_file_if_exists(os.path.join(output_abs, 'sim.out'))
+    _remove_file_if_exists(os.path.join(output_abs, 'sim.err'))
+    _remove_file_if_exists(os.path.join(output_abs, 'batch_script.sh'))
+
+
+def _ensure_dir_exists(dir_path):
+    """Ensure a directory exists, create it if not."""
+    if not os.path.exists(dir_path):
+        os.makedirs(os.path.join(dir_path, 'data/'))
+
+
+def _remove_file_if_exists(file_path):
+    """Remove file if it exists."""
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f'Removed file {file_path}')
+
+
+def generate_run_command(model, **kwargs):
+    libpath = struphy.__path__[0]
+    input_abs = kwargs.get('input_abs')
+    output_abs = kwargs.get('output_abs')
+    runtime = kwargs.get('runtime')
+    save_step = kwargs.get('save_step')
+    restart = kwargs.get('restart')
+    mpi = kwargs.get('mpi')
+    debug = kwargs.get('debug')
+    cprofile = kwargs.get('cprofile')
+    likwid = kwargs.get('likwid')
+    likwid_inp = kwargs.get('likwid_inp')
+    likwid_input_abs = kwargs.get('likwid_input_abs')
+    batch = kwargs.get('batch')
+    batch_abs = kwargs.get('batch_abs')
+    state = load_state(libpath)
+    if cprofile:
+        print('\nCprofile turned on.')
+    else:
+        print('\nCprofile turned off.')
+    
+    # command parts
+    cmd_python = ['python3']
+    cmd_main = ['main.py',
+                model,
+                '-i',
+                input_abs,
+                '-o',
+                output_abs,
+                '--runtime',
+                str(runtime),
+                '-s',
+                str(save_step)]
+    cmd_cprofile = ['-m',
+                    'cProfile',
+                    '-o',
+                    os.path.join(output_abs, 'profile_tmp'),
+                    '-s',
+                    'time']
+    if debug:
+        print('\nLaunching main() in Cobra debug mode ...')
+        return _generate_debug_command(cmd_python, cmd_main, cmd_cprofile, mpi, restart)
+    
+    if batch is not None or batch_abs is not None or likwid:
+        cmd_main[0] = os.path.join(libpath, cmd_main[0])
+
+    if likwid:
+        likwid_command = generate_likwid_command(mpi, state['i_path'], likwid_inp, likwid_input_abs)
+        return likwid_command + cmd_python + cprofile*cmd_cprofile + cmd_main + ['--likwid'] +  ['-r' * restart]
+    
+    print('\nLaunching main() in normal mode ...')
+
+    return ['mpirun','-n', str(mpi)] + cmd_python + cprofile*cmd_cprofile + cmd_main + ['-r' * restart]
+
+def _generate_debug_command(cmd_python, cmd_main, cmd_cprofile, mpi, restart):
+    """Generate the debug run command."""
+    print('\nLaunching main() in Cobra debug mode...')
+    return [
+        'srun', '-n', str(mpi), '-p', 'interactive', '--time', '119', '--mem', '2000'
+    ] + cmd_python + cmd_cprofile + cmd_main + ['-r' * restart]
+
+def generate_batch_script(model, **kwargs):
+    output_abs = kwargs.get('output_abs', None)
+    batch_abs = kwargs.get('batch_abs', None)
+    mpi = kwargs.get('mpi', 1)
+    likwid = kwargs.get('likwid', False)
+    likwid_repetitions = kwargs.get('likwid_repetitions', 1)
+
+    # copy batch script to output folder
+    batch_abs_new = os.path.join(output_abs, 'batch_script.sh')
+    shutil.copy2(batch_abs, batch_abs_new)
+
+    # delete srun command from batch script
+    with open(batch_abs_new, 'r') as f:
+        lines = f.readlines()
+        if 'srun' in lines[-1]:
+            lines = lines[:-2]
+
+    with open(batch_abs_new, 'w') as f:
+
+        for line in lines:
+            f.write(line)
+        f.write('# Run command added by Struphy\n')
+        
+        
+        command = generate_run_command(model, **kwargs)
+        print(f'{command = }')
+        if likwid:
+            print('Running with likwid')
+            f.write(f'# Launching likwid {likwid_repetitions} times with likwid-mpirun\n')
+            for i in range(likwid_repetitions):
+                f.write(f'\n\n# Run number {i:03}\n')
+                f.write(' '.join(command) + ' > ' + os.path.join(output_abs, f'struphy_likwid_{i:03}.out'))
+        else:
+            print('Running with srun')
+            command = ['srun'] + command[3:] #TODO
+            f.write(' '.join(command) + ' > ' + os.path.join(output_abs, 'struphy.out'))
+    return command
+def struphy_run(model, **kwargs):
     """
     Run a Struphy model: prepare arguments, output folder and execute main().
 
@@ -71,225 +307,27 @@ def struphy_run(model,
     likwid_repetitions : int, optional
         Number of repetitions for Likwid profiling. Default is 1.
     """
-
-    import subprocess
-    import shutil
-    import os
-    import struphy
-    import yaml
-
+    # Set default values for the arguments
+    kwargs = update_kwargs_inplace(**kwargs)
     libpath = struphy.__path__[0]
 
-    # Struphy paths
-    with open(os.path.join(libpath, 'state.yml')) as f:
-        state = yaml.load(f, Loader=yaml.FullLoader)
-
-    i_path = state['i_path']
-    o_path = state['o_path']
-    b_path = state['b_path']
-
-    # create absolute i/o paths
-    if input_abs is None:
-        if inp is None:
-            default_yml = os.path.join(i_path, f'params_{model}.yml')
-            if os.path.isfile(default_yml):
-                print('\nRunning with default parameter file ...')
-                input_abs = default_yml
-            else:
-                # load model class
-                from struphy.models import fluid, kinetic, hybrid, toy
-                objs = [fluid, kinetic, hybrid, toy]
-                for obj in objs:
-                    try:
-                        model_class = getattr(obj, model)
-                    except AttributeError:
-                        pass
-
-                params = model_class.generate_default_parameter_file()
-                exit()
-        else:
-            input_abs = os.path.join(i_path, inp)
-
-    if output_abs is None:
-        output_abs = os.path.join(o_path, output)
-
-    if batch_abs is None:
-        if batch is not None:
-            batch_abs = os.path.join(b_path, batch)
-
-    # take existing parameter file for restart
-    if restart:
-        input_abs = os.path.join(output_abs, 'parameters.yml')
-
-
-    # Read likwid params
-    if likwid:
-        if likwid_inp is None and likwid_input_abs is None:
-            # use default likwid parameters
-            likwid_command = ['likwid-mpirun', '-n', str(mpi), '-g', 'MEM_DP', '-stats', '-marker']
-        else:
-            if likwid_inp is not None:
-                likwid_input_abs = os.path.join(i_path, likwid_inp)
-            
-            
-            with open(likwid_input_abs, 'r') as file:
-                config = yaml.safe_load(file)
-            
-            # Get the command from the configuration
-            command_base = config.get('command', None)
-            if not command_base:
-                print("Missing required configuration: 'command'")
-                exit(1)
-            
-            likwid_config = config.get(command_base, {})
-            
-            # Get the options list
-            options = likwid_config.get('options', [])
-            
-            # Flatten the options list
-            flattened_options = ['-np',str(mpi)]
-            for item in options:
-                if isinstance(item, dict):
-                    for key, value in item.items():
-                        flattened_options.append(key)
-                        flattened_options.append(str(value))  # Ensure the value is a string
-                else:
-                    flattened_options.append(item)
-
-            
-            # Construct the command as a list
-            likwid_command = [command_base]
-            likwid_command.extend(flattened_options)
-
-    # command parts
-    cmd_python = ['python3']
-    cmd_main = ['main.py',
-                model,
-                '-i',
-                input_abs,
-                '-o',
-                output_abs,
-                '--runtime',
-                str(runtime),
-                '-s',
-                str(save_step)]
-    cmd_cprofile = ['-m',
-                    'cProfile',
-                    '-o',
-                    os.path.join(output_abs, 'profile_tmp'),
-                    '-s',
-                    'time']
+    kwargs['input_abs'], kwargs['output_abs'], kwargs['batch_abs'] = generate_io_path(model, libpath, **kwargs)
 
     # run in normal or debug mode
-    if batch_abs is None:
-
-        if debug:
-            print('\nLaunching main() in Cobra debug mode ...')
-            command = ['srun',
-                       '-n',
-                       str(mpi),
-                       '-p',  # interactive commands
-                       'interactive',
-                       '--time',
-                       '119',
-                       '--mem',
-                       '2000'] + cmd_python + cprofile*cmd_cprofile + cmd_main
-        elif likwid:
-            cmd_main[0] = f"{libpath}/{cmd_main[0]}"
-            command = likwid_command + cmd_python + cprofile*cmd_cprofile + cmd_main + ['--likwid']
-        else:
-            print('\nLaunching main() in normal mode ...')
-            command = ['mpirun',
-                       '-n',
-                       str(mpi)] + cmd_python + cprofile*cmd_cprofile + cmd_main
-
-        # add restart flag
-        if restart:
-            command += ['-r']
-            
-        if cprofile:
-            print('\nCprofile turned on.')
-        else:
-            print('\nCprofile turned off.')
-
+    if kwargs['batch_abs'] is None:
+        command = generate_run_command(model, **kwargs)
         # run command as subprocess
         print(f"\nRunning the following command:\n{' '.join(command)}")
-        if likwid:
-            subprocess.run(command, check=True)
-        else:
-            subprocess.run(command, check=True, cwd=libpath)
-        
+        subprocess.run(command, check=True, cwd=libpath)
     # run in batch mode
     else:
-
-        # create output folder if it does not exit
-        if not os.path.exists(output_abs):
-            os.mkdir(output_abs)
-            os.mkdir(os.path.join(output_abs, 'data/'))
-
-        # remove sim.out file
-        file = os.path.join(output_abs, 'sim.out')
-        if os.path.exists(file):
-            os.remove(file)
-            print('Removed file ' + file)
-
-        # remove sim.err file
-        file = os.path.join(output_abs, 'sim.err')
-        if os.path.exists(file):
-            os.remove(file)
-            print('Removed file ' + file)
-
-        # remove old batch script
-        file = os.path.join(output_abs, 'batch_script.sh')
-        if os.path.exists(file):
-            os.remove(file)
-            print('Removed file ' + file)
-
-        # remove struphy.out file
-        file = os.path.join(output_abs, 'sim.out')
-        if os.path.exists(file):
-            os.remove(file)
-            print('Removed file ' + file)
-
-        # copy batch script to output folder
-        batch_abs_new = os.path.join(output_abs, 'batch_script.sh')
-        shutil.copy2(batch_abs, batch_abs_new)
-
-        # delete srun command from batch script
-        with open(batch_abs_new, 'r') as f:
-            lines = f.readlines()
-            if 'srun' in lines[-1]:
-                lines = lines[:-2]
-
-        with open(batch_abs_new, 'w') as f:
-
-            for line in lines:
-                f.write(line)
-            f.write('# Run command added by Struphy\n')
-            
-            
-            command = cmd_python + cprofile*cmd_cprofile + [f"{libpath}/{' '.join(cmd_main)}"]
-            if restart:
-                command += ['-r']
-            
-            if likwid:
-                command = likwid_command + command + ['--likwid']
-            
-            if likwid:
-                print('Running with likwid')
-                f.write(f'# Launching likwid {likwid_repetitions} times with likwid-mpirun\n')
-                for i in range(likwid_repetitions):
-                    f.write(f'\n\n# Run number {i:03}\n')
-                    f.write(' '.join(command) + ' > ' + os.path.join(output_abs, f'struphy_likwid_{i:03}.out'))
-            else:
-                print('Running with srun')
-                command = ['srun'] + command
-                f.write(' '.join(command) + ' > ' + os.path.join(output_abs, 'struphy.out'))
+        cleanup_out_dir(kwargs['output_abs'])
+        command = generate_batch_script(model, **kwargs)
         
         # submit batch script in output folder
         print('\nLaunching main() in batch mode ...')
         subprocess.run(['sbatch',
                         'batch_script.sh',
                         ],
-                       check=True, cwd=output_abs)
+                       check=True, cwd=kwargs['output_abs'])
     return command
