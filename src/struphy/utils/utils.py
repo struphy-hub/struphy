9b01cd4ab841b6a0ed46f1febe212c204dd4be82
Max <max.lindqvist@ipp.mpg.de>
2024-10-17 11:51:46 +0200

    Added a utils module with the function read_state()

---
+++ src/struphy/utils/utils.py
@@ -0,0 +1,43 @@
+import os
+import yaml
+import struphy
+
+def read_state():
+    """
+    Reads the 'state.yml' file located in the Struphy library path.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing the parsed YAML content of the 'state.yml' file.
+    
+    Raises:
+    -------
+    FileNotFoundError
+        If the 'state.yml' file is not found.
+    YAMLError
+        If there is an error while parsing the YAML file.
+    """
+
+    # Get the path to the Struphy library
+    LIB_PATH = struphy.__path__[0]
+    
+    state_file = os.path.join(LIB_PATH, 'state.yml')
+    
+    try:
+        with open(state_file, 'r') as f:
+            state = yaml.load(f, Loader=yaml.FullLoader)
+    except FileNotFoundError as e:
+        print(f"Error: The file '{state_file}' was not found.")
+        raise e
+    except yaml.YAMLError as e:
+        print(f"Error parsing the YAML file: {e}")
+        raise e
+    
+    return state
+
+
+if __name__ == '__main__':
+    state = read_state()
+    for k,val in state.items():
+        print(k,val)
