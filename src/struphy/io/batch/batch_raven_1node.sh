0fbc1c537fac6f86c4a307b6eda095fb0d4a4d29
Max <max.lindqvist@ipp.mpg.de>
2024-10-18 16:29:02 +0200

    Added batch_raven_1node.sh

---
+++ src/struphy/io/batch/batch_raven_1node.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#SBATCH -o ./job_struphy_%j.out
+#SBATCH -e ./job_struphy_%j.err
+#SBATCH -D ./
+#SBATCH -J job_struphy
+#SBATCH --nodes=1                # number of compute nodes
+#SBATCH --ntasks-per-node=72     # number of MPI processes (max 144 on raven with hyperthreading)
+##SBATCH --mail-type=
+##SBATCH --mail-user=
+#SBATCH --time=00:10:00
+
+# Load modules
+module purge
+module load gcc/12 openmpi/4.1 anaconda/3/2023.03 git/2.43 pandoc/3.1 likwid/5.2
+
+# Set up environment
+source /u/maxlin/virtual_envs/env_struphy/bin/activate
+LIKWID_PREFIX=$(realpath $(dirname $(which likwid-topology))/..)
+export LD_LIBRARY_PATH=$LIKWID_PREFIX/lib
+export LD_LIBRARY_PATH=/mpcdf/soft/SLE_15/packages/skylake/likwid/gcc_12-12.1.0/5.3.0/lib:$LD_LIBRARY_PATH
+
+# Set the number of OMP threads *per process* to avoid overloading of the node!
+#export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# For pinning threads correctly
+#export OMP_PLACES=cores
+KMP_AFFINITY=scatter
+
