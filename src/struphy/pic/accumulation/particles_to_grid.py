d085da118dd892395ff2ace73b6b107ca6baba3d
Byung Kyu Na <byungkyu.na@ipp.mpg.de>
2024-11-22 11:52:07 +0100

    fix hybrid filtering

--- src/struphy/pic/accumulation/particles_to_grid.py
+++ src/struphy/pic/accumulation/particles_to_grid.py
@@ -227,7 +227,7 @@ class Accumulator:
 
                     for _ in range(self.filter_params["repeat"]):
                         for i in range(3):
-                            filters.apply_three_point_filter(
+                            filters.apply_three_point_filter_3d(
                                 vec[i]._data,
                                 np.array(self.derham.Nel),
                                 np.array(self.derham.spl_kind),
@@ -245,7 +245,7 @@ class Accumulator:
 
                     for _ in range(self.filter_params["repeat"]):
                         for i in range(2):
-                            filters.apply_three_point_filter(
+                            filters.apply_three_point_filter_2d(
                                 vec[i]._data,
                                 np.array(self.derham.Nel),
                                 np.array(self.derham.spl_kind),
