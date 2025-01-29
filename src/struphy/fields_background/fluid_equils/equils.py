8dac0a9bf1c11e8901f7cc95a345e54fee1b2f43
g-philippthalhof <ph.thalhofer@gmail.com>
2025-01-29 09:49:16 +0000

    Added Background Analytic solution of Restelli paper

--- src/struphy/fields_background/fluid_equils/equils.py
+++ src/struphy/fields_background/fluid_equils/equils.py
@@ -47,3 +47,42 @@ class ConstantVelocity(FluidEquilibrium):
             return self.params["n"] + 0 * x
         elif self.params["density_profile"] == "affine":
             return self.params["n"] + self.params["n1"] * x
+
+
+
+class AnalyticSolutionRestelli(FluidEquilibrium):
+    r"""Base class for a constant distribution function on the unit cube.
+
+    """
+
+    def __init__(self, **params):
+        params_default = {"R0": 2.0, "a": 1.0, "B0": 10, "Bp": 12.5, "alpha": 0.1, "beta": 1.0}
+
+        self._params = set_defaults(params, params_default)
+
+    @property
+    def params(self):
+        """Parameters dictionary."""
+        return self._params
+
+    # equilibrium ion velocity
+    def u_xyz(self, x, y, z):
+        """Velocity of ions and electrons."""
+        ux = self.params["alpha"]*np.sqrt(x**2+y**2)/(self.params["a"]*self.params["R0"])*(-z) + self.params["beta"]*self.params["Bp"]*self.params["R0"]/(self.params["B0"]*self.params["a"]*np.sqrt(x**2+y**2))*z
+        uy = self.params["alpha"]*np.sqrt(x**2+y**2)/(self.params["a"]*self.params["R0"])*(np.sqrt(x**2+y**2)-self.params["R0"]) + self.params["beta"]*self.params["Bp"]*self.params["R0"]/(self.params["B0"]*self.params["a"]*np.sqrt(x**2+y**2))*(-(np.sqrt(x**2+y**2)-self.params["R0"]))
+        uz = self.params["beta"]*self.params["Bp"]*self.params["R0"]/(self.params["B0"]*self.params["a"]*np.sqrt(x**2+y**2))*self.params["B0"]*self.params["a"]/self.params["Bp"]
+
+        return ux, uy, uz
+
+    # equilibrium pressure
+    def p_xyz(self, x, y, z):
+        """Plasma pressure."""
+        pp = 0 * x 
+
+        return pp
+
+    # equilibrium number density
+    def n_xyz(self, x, y, z):
+        """Number density."""
+        
+        return 0 * x
