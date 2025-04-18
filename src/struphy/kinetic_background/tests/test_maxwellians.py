d29da44490dda7c7b583fb60442a945933b9db57
g-philippthalhof <ph.thalhofer@gmail.com>
2025-04-18 12:41:24 +0000

    FluxAlignedTokamak domain for testing

--- src/struphy/kinetic_background/tests/test_maxwellians.py
+++ src/struphy/kinetic_background/tests/test_maxwellians.py
@@ -348,6 +348,8 @@ def test_maxwellian_3d_mhd(Nel, with_desc, show_plot=False):
                 )
             elif "EQDSKequilibrium" in key:
                 mhd_equil.domain = domains.Tokamak(equilibrium=mhd_equil)
+            elif "FluxAlignedTokamak" in key:
+                mhd_equil.domain = domains.Tokamak(equilibrium=mhd_equil)
             elif "HomogenSlab" in key:
                 mhd_equil.domain = domains.Cuboid()
             elif "ShearedSlab" in key:
