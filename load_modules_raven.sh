36acde2b713933cf1a39bd134fd6556584aa7b85
maxlin <max.lindqvist@ipp.mpg.de>
2025-06-11 10:02:04 +0200

    Added load_modules_raven.sh

---
+++ load_modules_raven.sh
@@ -0,0 +1,11 @@
+module purge
+module load nvhpcsdk/25 cuda/12.6-nvhpcsdk_25 openmpi/5.0 openmpi_gpu/5.0 gcc/13
+
+source ~/virtual_envs/env_struphy_gpu/bin/activate
+
+# Add likwid module
+
+module load likwid/5.3
+LIKWID_PREFIX=$(realpath $(dirname $(which likwid-topology))/..)
+export LD_LIBRARY_PATH=$LIKWID_PREFIX/lib:$LD_LIBRARY_PATH
+
