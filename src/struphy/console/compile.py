9c997c6cf2510bb04d2842789623f5958bb54f9b
Max <max.lindqvist@ipp.mpg.de>
2025-06-18 10:09:16 +0200

    bugfix

--- src/struphy/console/compile.py
+++ src/struphy/console/compile.py
@@ -202,9 +202,9 @@ def struphy_compile(language, compiler, compiler_config, omp_pic, omp_feec, dele
         flags = "--language=" + language
 
         if compiler_config:
-            flags += " --compiler-config=" + compiler_config
+            flags += [" --compiler-config=" + compiler_config]
         else:
-            flags += " --compiler-family=" + compiler
+            flags += [" --compiler-family=" + compiler]
 
         if time_execution:
             flags += " --time_execution"
@@ -298,9 +298,9 @@ def struphy_compile(language, compiler, compiler_config, omp_pic, omp_feec, dele
             "--language=" + language,
         ]
         if compiler_config:
-            cmd += "--compiler-config=" + compiler_config
+            cmd += ["--compiler-config=" + compiler_config]
         else:
-            cmd += "--compiler-family=" + compiler
+            cmd += ["--compiler-family=" + compiler]
 
         subp_run(cmd)
 
