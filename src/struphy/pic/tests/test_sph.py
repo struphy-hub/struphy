09d39418ad07920c912fbd1a8a5c3d62880302c6
Amin Raiessi <rasa@ipp.mpg.de>
2025-07-11 18:46:22 +0200

    SPH test MC/tesselation final

--- src/struphy/pic/tests/test_sph.py
+++ src/struphy/pic/tests/test_sph.py
@@ -525,6 +525,6 @@ if __name__ == "__main__":
     
     #test_evaluation_SPH_Np_convergence_1d((12,1,1), "periodic", tesselation=True, show_plot=True)
     #test_evaluation_SPH_h_convergence_1d((8,1,1), "periodic", tesselation = True, show_plot=True)
-    test_evaluation_mc_Np_and_h_convergence_1d((16,1,1),"periodic",tesselation = True,  show_plot=True)
+    test_evaluation_mc_Np_and_h_convergence_1d((16,1,1),"periodic",tesselation = False,  show_plot=True)
     #test_evaluation_SPH_Np_convergence_2d((16,16,1), "periodic", "periodic", tesselation = False , show_plot=True)
     
