cda3fa0f309f214ab31e05610792cdf7edb4550e
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-07-17 16:12:41 +0200

    new folder `physics`; holds the natural constants for the moment

---
+++ src/struphy/physics/physics.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class ConstantsOfNature:
+    e = 1.602176634e-19  # elementary charge (C)
+    mH = 1.67262192369e-27  # proton mass (kg)
+    mu0 = 1.25663706212e-6  # magnetic constant (N/A^2)
+    eps0 = 8.8541878128e-12  # vacuum permittivity (F/m)
+    kB = 1.380649e-23  # Boltzmann constant (J/K)
+    c = 299792458  # speed of light (m/s)
