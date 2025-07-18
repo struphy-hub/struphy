2b7812bcb761c35889ea4aa0f0bc3b6a8fc17398
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-07-18 15:28:38 +0200

    working on struphy main.py

--- src/struphy/io/parameters.py
+++ src/struphy/io/parameters.py
@@ -1,78 +1,70 @@
-from struphy.fields_background.base import FluidEquilibrium
-from struphy.geometry.base import Domain
-from struphy.io.options import DerhamOptions, Time, Units
-from struphy.topology import grids
+from __future__ import annotations
+from mpi4py import MPI
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from struphy.fields_background.base import FluidEquilibrium
+    from struphy.geometry.base import Domain
+    from struphy.io.options import DerhamOptions, Time, Units
+    from struphy.topology import grids
+    from struphy.models.base import StruphyModel
 
 
 class StruphyParameters:
-    """Wrapper around Struphy simulation parameters."""
+    """Wrapper around simulation parameters."""
 
     def __init__(
         self,
-        model: str = None,
-        domain: Domain = None,
-        grid: grids.TensorProductGrid = None,
-        equil: FluidEquilibrium = None,
+        model: StruphyModel = None,
         units: Units = None,
+        domain: Domain = None,
+        equil: FluidEquilibrium = None,
         time: Time = None,
+        grid: grids.TensorProductGrid = None,
         derham: DerhamOptions = None,
-        em_fields=None,
-        fluid=None,
-        kinetic=None,
-        diagnostic_fields=None,
+        verbose: bool = False
     ):
         self._model = model
-        self._domain = domain
-        self._grid = grid
-        self._equil = equil
         self._units = units
+        self._domain = domain
+        self._equil = equil
         self._time = time
+        self._grid = grid
         self._derham = derham
-        self._em_fields = em_fields
-        self._fluid = fluid
-        self._kinetic = kinetic
-        self._diagnostic_fields = diagnostic_fields
+        
+        if verbose and MPI.COMM_WORLD.Get_rank() == 0:
+            print(f"{self.model = }")
+            print(f"{self.units = }")
+            print(f"{self.domain = }")
+            print(f"{self.equil = }")
+            print(f"{self.time = }")
+            print(f"{self.grid = }")
+            print(f"{self.derham = }")
 
     @property
     def model(self):
         return self._model
 
     @property
+    def units(self):
+        return self._units
+    
+    @property
     def domain(self):
         return self._domain
 
+    @property
+    def equil(self):
+        return self._equil
+    
+    @property
+    def time(self):
+        return self._time
+    
     @property
     def grid(self):
         return self._grid
 
     @property
-    def equil(self):
-        return self._equil
-
-    @property
-    def units(self):
-        return self._units
-
-    @property
-    def time(self):
-        return self._time
-
-    @property
     def derham(self):
         return self._derham
-
-    @property
-    def em_fields(self):
-        return self._em_fields
-
-    @property
-    def fluid(self):
-        return self._fluid
-
-    @property
-    def kinetic(self):
-        return self._kinetic
-
-    @property
-    def diagnostic_fields(self):
-        return self._diagnostic_fields
