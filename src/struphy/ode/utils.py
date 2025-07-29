ae456843644099587cc6cf60bd000cfe123caa8f
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-07-29 08:15:08 +0200

    make ButcherTableau a dataclass

--- src/struphy/ode/utils.py
+++ src/struphy/ode/utils.py
@@ -1,6 +1,19 @@
-from struphy.utils.arrays import xp as np
+import numpy as np
+from dataclasses import dataclass
+from typing import Literal, get_args
 
 
+OptsButcher = Literal[
+            "rk4",
+            "forward_euler",
+            "heun2",
+            "rk2",
+            "heun3",
+            "3/8 rule",
+        ]
+
+
+@dataclass
 class ButcherTableau:
     r"""
     Butcher tableau for explicit s-stage Runge-Kutta methods.
@@ -13,50 +26,40 @@ class ButcherTableau:
 
     Parameters
     ----------
-    algo : str
+    algo : OptsButcher
         Name of the RK method.
     """
+    
+    algo: OptsButcher = "rk4"
 
-    @staticmethod
-    def available_methods() -> list:
-        meth_avail = [
-            "rk4",
-            "forward_euler",
-            "heun2",
-            "rk2",
-            "heun3",
-            "3/8 rule",
-        ]
-        return meth_avail
-
-    def __init__(self, algo: str = "rk4"):
+    def __post_init__(self):
         # choose algorithm
-        if algo == "forward_euler":
+        if self.algo == "forward_euler":
             a = ()
             b = (1.0,)
             c = (0.0,)
             conv_rate = 1
-        elif algo == "heun2":
+        elif self.algo == "heun2":
             a = ((1.0,),)
             b = (1 / 2, 1 / 2)
             c = (0.0, 1.0)
             conv_rate = 2
-        elif algo == "rk2":
+        elif self.algo == "rk2":
             a = ((1 / 2,),)
             b = (0.0, 1.0)
             c = (0.0, 1 / 2)
             conv_rate = 2
-        elif algo == "heun3":
+        elif self.algo == "heun3":
             a = ((1 / 3,), (0.0, 2 / 3))
             b = (1 / 4, 0.0, 3 / 4)
             c = (0.0, 1 / 3, 2 / 3)
             conv_rate = 3
-        elif algo == "rk4":
+        elif self.algo == "rk4":
             a = ((1 / 2,), (0.0, 1 / 2), (0.0, 0.0, 1.0))
             b = (1 / 6, 1 / 3, 1 / 3, 1 / 6)
             c = (0.0, 1 / 2, 1 / 2, 1.0)
             conv_rate = 4
-        elif algo == "3/8 rule":
+        elif self.algo == "3/8 rule":
             a = ((1 / 3,), (-1 / 3, 1.0), (1.0, -1.0, 1.0))
             b = (1 / 8, 3 / 8, 3 / 8, 1 / 8)
             c = (0.0, 1 / 3, 2 / 3, 1.0)
@@ -78,7 +81,7 @@ class ButcherTableau:
 
         self._conv_rate = conv_rate
 
-    __available_methods__ = available_methods()
+    __available_methods__ = get_args(OptsButcher)
 
     @property
     def a(self):
