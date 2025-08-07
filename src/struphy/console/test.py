2bd1a8710c16e1ccb4a6f8796500737ba3f52f65
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-08-07 10:02:48 +0200

    minor changes

--- src/struphy/console/test.py
+++ src/struphy/console/test.py
@@ -4,7 +4,7 @@ from struphy.utils.utils import subp_run
 def struphy_test(
     group: str,
     *,
-    mpi: int = 2,
+    mpi: int = 4,
     fast: bool = False,
     with_desc: bool = False,
     Tend: float = None,
@@ -22,7 +22,7 @@ def struphy_test(
         Test identifier: "unit", "models", "fluid", "kinetic", "hybrid", "toy" or a model name.
 
     mpi : int
-        Number of MPI processes used in tests (must be >1, default=2).
+        Number of MPI processes used in tests (must be >1, default=4).
 
     fast : bool
         Whether to test models just in slab geometry.
