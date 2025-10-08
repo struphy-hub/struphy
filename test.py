101e03bfffa53fb17ff4e488946bc96eef8b2c77
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-10-08 14:55:10 +0200

    test

---
+++ test.py
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+from struphy.fields_background import equils
+from struphy.fields_background.base import FluidEquilibrium, FluidEquilibriumWithB
+
+=======
+from struphy.fields_background.base import FluidEquilibriumWithB
+>>>>>>> devel
