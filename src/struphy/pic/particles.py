bdd2be3727b7bd166541082e8839baed2b40d1f1
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-10-08 14:52:47 +0200

    test

--- src/struphy/pic/particles.py
+++ src/struphy/pic/particles.py
@@ -5,6 +5,7 @@ import numpy as np
 <<<<<<< HEAD
 from struphy.fields_background import equils
 from struphy.fields_background.base import FluidEquilibrium, FluidEquilibriumWithB
+
 =======
 from struphy.fields_background.base import FluidEquilibriumWithB
 >>>>>>> devel
