d9fbee4af58750add8fe30904d6a9f3b535712ff
Max <max.lindqvist@ipp.mpg.de>
2025-10-08 21:54:15 +0200

    Added typing magic so that autocompletion works with xp

--- src/struphy/xp.py
+++ src/struphy/xp.py
@@ -1,8 +1,18 @@
 import os
+from types import ModuleType
+from typing import TYPE_CHECKING, Literal
+
+from numpy import isin
+
+BackendType = Literal["numpy", "cupy"]
 
 
 class ArrayBackend:
-    def __init__(self, backend: str = "numpy") -> None:
+    def __init__(
+        self,
+        backend: BackendType = "numpy",
+        verbose: bool = False,
+    ) -> None:
         self._backend = backend
 
         # Import numpy/cupy
@@ -10,23 +20,24 @@ class ArrayBackend:
             try:
                 import cupy as cp
 
-                self._xp =  cp
+                self._xp = cp
             except ImportError:
-                print("CuPy not available, falling back to NumPy.")
+                if verbose:
+                    print("CuPy not available, falling back to NumPy.")
                 self._backend = "numpy"
 
         if self.backend == "numpy":
             import numpy as np
 
             self._xp = np
-            print(f"{self._xp = }")
 
     @property
-    def backend(self) -> str:
+    def backend(self) -> BackendType:
         return self._backend
 
     @property
-    def xp(self):
+    def xp(self) -> ModuleType:
+        assert isinstance(self._xp, ModuleType)
         return self._xp
 
 
@@ -35,6 +46,9 @@ array_backend = ArrayBackend(
     backend=os.getenv("ARRAY_BACKEND", "numpy").lower(),
 )
 
-xp = array_backend.xp
+if TYPE_CHECKING:
+    import numpy as xp
+else:
+    xp = array_backend.xp
 
 print(f"Using {xp.__name__} backend.")
