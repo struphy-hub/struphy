5aca37fd5212840842b921f797b51c2a5cb3190d
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-10-15 19:35:33 +0200

    Restructured MPIwrapper and MockMPI classes

--- src/struphy/utils/mpi.py
+++ src/struphy/utils/mpi.py
@@ -3,41 +3,78 @@ from dataclasses import dataclass
 from time import time
 from typing import TYPE_CHECKING
 
+
+# Might not be needed
+class MPICommWrapper:
+    def __init__(self, use_mpi=True):
+        self.use_mpi = use_mpi
+        if use_mpi:
+            from mpi4py import MPI
+            self.comm = MPI.COMM_WORLD
+        else:
+            self.comm = MockComm()
+
+    def __getattr__(self, name):
+        return getattr(self.comm, name)
+
 class MockComm:
-    
-    @classmethod
-    def Get_rank():
+    def __getattr__(self, name):
+        # Return a function that does nothing and returns None
+        def dummy(*args, **kwargs):
+            return None
+        return dummy
+
+    # Override some functions
+    def Get_rank(self):
         return 0
     
-    @classmethod
-    def Get_size():
+    def Get_size(self):
         return 1
 
 
-@dataclass
-class MockMPI:
-    COMM_WORLD = MockComm
+# class MockComm:
     
-    def Wtime(self):
-        return time()
+#     @classmethod
+#     def Get_rank():
+#         return 0
+    
+#     @classmethod
+#     def Get_size():
+#         return 1
+
+
+# @dataclass
+# class MockMPI:
+#     COMM_WORLD = MockComm
+    
+#     def Wtime(self):
+#         return time()
 
 
 class MPIwrapper:
     def __init__(self, use_mpi: bool = False):
+        self.use_mpi = use_mpi
         if use_mpi:
-            try:
-                from mpi4py import mpi as MPI 
-            except ModuleNotFoundError:
-                MPI = MockMPI()
+            from mpi4py import MPI
+            self._MPI = MPI.COMM_WORLD
         else:
-            MPI = MockMPI()
-            
-        self._MPI = MPI
-        
+            self._MPI = MockMPI()
+
     @property
     def MPI(self):
         return self._MPI
-    
+
+class MockMPI:
+    def __getattr__(self, name):
+        # Return a function that does nothing and returns None
+        def dummy(*args, **kwargs):
+            return None
+        return dummy
+
+    # Override some functions
+    @property
+    def COMM_WORLD(self):
+        return MockComm()
 
 # TODO: add environment variable for mpi use
 mpi_wrapper = MPIwrapper()
