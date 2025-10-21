25f0deada1a34aa021b6c7af855b6fb19728348f
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-10-21 15:20:55 +0200

    use Barrier() in main.py

--- src/struphy/main.py
+++ src/struphy/main.py
@@ -172,7 +172,7 @@ def run(
                 clone_config.print_particle_config()
 
     model.clone_config = clone_config
-    comm.Barrier()
+    Barrier()
 
     ## configure model instance
 
@@ -213,7 +213,7 @@ def run(
     if rank < 32:
         if rank == 0:
             print("")
-        comm.Barrier()
+        Barrier()
         print(f"Rank {rank}: executing main.run() for model {model_name} ...")
 
     if size > 32 and rank == 32:
