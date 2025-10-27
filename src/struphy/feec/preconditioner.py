c37e50a7454580d2c8e1cd8686dc23dd73437c0b
Max <max.lindqvist@ipp.mpg.de>
2025-10-27 19:21:32 +0100

    Removed duplicate 'def domain'

--- src/struphy/feec/preconditioner.py
+++ src/struphy/feec/preconditioner.py
@@ -319,11 +319,6 @@ class MassMatrixPreconditioner(LinearOperator):
         return self._solver
 
     @property
-    def domain(self):
-        """The domain of the linear operator - an element of Vectorspace"""
-        return self._space
-
-    @property
     def codomain(self):
         """The codomain of the linear operator - an element of Vectorspace"""
         return self._codomain
@@ -704,9 +699,6 @@ class MassMatrixDiagonalPreconditioner(LinearOperator):
     def solver(self):
         """KroneckerLinearSolver or BlockDiagonalSolver for exactly inverting the approximate mass matrix self.matrix."""
         return self._solver
-
-    @property
-    def domain(self):
         """The domain of the linear operator - an element of Vectorspace"""
         return self._space
 
