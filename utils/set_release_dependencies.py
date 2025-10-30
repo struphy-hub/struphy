bdd43080c2199dbd85bb9dc536674e75924e99bd
Max <max.lindqvist@ipp.mpg.de>
2025-10-30 17:41:15 +0100

    Add formatting check of the notebooks (#76)
    Redo of https://github.com/struphy-hub/struphy/pull/64 (with a few extra
    edits of the CI)
    
    **Solves the following issue(s):**
    
    Closes #54 
    
    - Formatted all the `.ipynb` files in the repo
    - Added the notebooks to the formatting checks in the CI
    
    ---------
    
    Co-authored-by: Stefan Possanner <stefan.possanner@ipp.mpg.de>
    Co-authored-by: Byung Kyu Na <byungkyu.na@ipp.mpg.de>
    Co-authored-by: Stefan Possanner <86720346+spossann@users.noreply.github.com>

--- utils/set_release_dependencies.py
+++ utils/set_release_dependencies.py
@@ -2,8 +2,6 @@ import importlib.metadata
 import re
 import tomllib
 
-import tomli_w
-
 
 def get_min_bound(entry):
     match = re.search(r"(>=|==|~=|>|>)\s*([\w\.\-]+)", entry)
@@ -42,7 +40,11 @@ def update_dependencies(dependencies):
         try:
             installed_version = importlib.metadata.version(package_name)
 
-            package_deps = {"installed": installed_version, "min": get_min_bound(entry), "max": get_max_bound(entry)}
+            package_deps = {
+                "installed": installed_version,
+                "min": get_min_bound(entry),
+                "max": get_max_bound(entry),
+            }
 
             if package_deps["installed"]:
                 dependencies[i] = generate_updated_entry(package_name, package_deps)
@@ -59,6 +61,8 @@ def update_dependencies(dependencies):
 
 def main():
     with open("pyproject.toml", "rb") as f:
+        import tomllib
+
         pyproject_data = tomllib.load(f)
 
     mandatory_dependencies = pyproject_data["project"]["dependencies"]
@@ -69,6 +73,8 @@ def main():
         update_dependencies(group_deps)
 
     with open("pyproject.toml", "wb") as f:
+        import tomli_w
+
         tomli_w.dump(pyproject_data, f)
 
 
