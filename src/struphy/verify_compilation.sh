109d950e0fab40c4ac3ed78683d5145637d6aea9
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-11-17 16:16:19 +0100

    Removed src/struphy/verify_compilation.sh

--- src/struphy/verify_compilation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-SO_EXT=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
-
-cd ..
-struphy_path=$(python3 -c "import struphy as _; print(_.__path__[0])")
