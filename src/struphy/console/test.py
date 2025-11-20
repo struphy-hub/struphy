b24ca966bcdfb9937f7b565aa77bb7ae5f4845da
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2025-11-20 15:15:57 +0100

    use run-attempt in cache name, use restore-keys

--- src/struphy/console/test.py
+++ src/struphy/console/test.py
@@ -44,8 +44,8 @@ def struphy_test(
         list_of_tests = [
             f"{LIBPATH}/bsplines/tests/",
             f"{LIBPATH}/console/tests/",
-            f"{LIBPATH}/feec/tests/",
-            f"{LIBPATH}/fields_background/tests/",
+            # f"{LIBPATH}/feec/tests/",
+            # f"{LIBPATH}/fields_background/tests/",
             # f"{LIBPATH}/geometry/tests/",
             # f"{LIBPATH}/initial/tests/",
             # f"{LIBPATH}/kinetic_background/tests/",
