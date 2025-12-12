b52040df440c6265c59a21f9d8a5b29d9fef2abe
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-12-12 13:22:57 +0100

    Added src/struphy/models/utils.py for utilities for retreiving models

---
+++ src/struphy/models/utils.py
@@ -0,0 +1,70 @@
+import inspect
+
+import struphy.models as models
+from struphy.models.base import StruphyModel
+from struphy.io.options import ModelTypes
+
+
+def get_model_by_name(model_name: str) -> type[StruphyModel]:
+    try:
+        model_class = getattr(models, model_name)
+    except AttributeError:
+        raise ModuleNotFoundError(f"{model_name} not found in models.")
+
+    if not issubclass(model_class, StruphyModel):
+        raise TypeError(f"{model_name} is not a StruphyModel subclass.")
+
+    return model_class
+
+def get_all_models(model_type: ModelTypes | None = None) -> list[type[StruphyModel]]:
+    model_classes = []
+
+    for name, obj in inspect.getmembers(models):
+        # Only include classes that are subclasses of StruphyModel, excluding StruphyModel itself
+        if inspect.isclass(obj) and issubclass(obj, StruphyModel) and obj is not StruphyModel:
+            if type is None or model_type == obj.model_type:
+                model_classes.append(obj)
+
+    return model_classes
+
+def get_all_model_names(model_type: ModelTypes | None = None) -> list[str]:
+    model_classes = get_all_models(model_type=model_type)
+
+    return [model.__name__ for model in model_classes]
+
+def get_model_type_message(model_type: ModelTypes) -> str:
+    models = get_all_models(model_type=model_type)
+
+    # fluid message
+    models_message = f"{model_type} models:\n"
+    models_message += "-------------\n"
+    models_message += "\n".join([mod.__name__ for mod in models])
+
+    return models_message
+
+def generate_models_message() -> str:
+
+    fluid_message = get_model_type_message(model_type="Fluid")
+    kinetic_message = get_model_type_message(model_type="Kinetic")
+    hybrid_message = get_model_type_message(model_type="Hybrid")
+
+    # model message
+    model_message = "run one of the following models:\n"
+    model_message += "\n" + fluid_message
+    model_message += "\n\n" + kinetic_message
+    model_message += "\n\n" + hybrid_message
+
+    return model_message
+
+if __name__ == "__main__":
+    fluid_models = get_all_models(model_type="Fluid")
+    kinetic_models = get_all_models(model_type="Kinetic")
+    hybrid_models = get_all_models(model_type="Hybrid")
+    
+    print(f"Fluid models: {[mod.__name__ for mod in fluid_models]}")
+    print(f"Kinetic_models: {[mod.__name__ for mod in kinetic_models]}")
+    print(f"Hybrid models: {[mod.__name__ for mod in hybrid_models]}")
+
+    model_message = generate_models_message()
+
+    print(model_message)
