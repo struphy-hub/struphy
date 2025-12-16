a368e1d854e306b0541715a773939419b2ddb486
Max <max.lindqvist@ipp.mpg.de>
2025-12-16 22:35:49 +0100

    Install pip install --no-binary=mpi4py mpi4py

--- .github/scripts/install_and_test.sh
+++ .github/scripts/install_and_test.sh
@@ -13,7 +13,8 @@ source env/bin/activate
 
 # Install Struphy
 pip install --upgrade pip
-pip install ".[dev,phys,doc]"
+pip install --no-binary=mpi4py mpi4py
+pip install ".[dev]"
 pip list
 struphy -h
 struphy --refresh-models
