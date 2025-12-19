2b806db4f02a0752b8af50d8b840d4ee6bd1f67c
Max <max.lindqvist@ipp.mpg.de>
2025-12-19 11:47:46 +0100

    Updated the modules

--- setup/modules.pitagora.sh
+++ setup/modules.pitagora.sh
@@ -1,6 +1,7 @@
 MODULES_INTEL="intel-oneapi-compilers-classic/2021.10.0 \
 intel-oneapi-mkl/2024.0.0--intel-oneapi-mpi--2021.12.1 \
-hdf5/1.14.3--intel-oneapi-mpi--2021.12.1--oneapi--2024.1.0 \
-python/3.11.7 \
-cmake/3.27.9 \
-netcdf-fortran/4.6.1--intel-oneapi-mpi--2021.12.1--oneapi--2024.1.0"
+python/3.11.7"
+
+MODULES_GCC="gcc/12.3.0 \
+openmpi/4.1.6--gcc--12.3.0 \
+python/3.11.7"
--- setup/modules.raven.sh
+++ setup/modules.raven.sh
@@ -1,9 +1,11 @@
 MODULES_INTEL="intel/21.7.1 \
 impi/2021.7 \
 python-waterboa/2024.06 \
-mpi4py/3.1.5 \
-cmake/4.0 \
-netcdf-serial/4.9.2 \
 mkl/2025.3 \
 hdf5-serial/1.14.1"
 
+MODULES_GCC="gcc/15 \
+openmpi/5.0 \
+python-waterboa/2025.06 \
+pandoc/3.1 \
+git/2.5.0"
