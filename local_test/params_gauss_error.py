05e548ea2e02f58028f65840bb54dba5b47d5753
Pititaras Attapon <pititaras@gmail.com>
2026-02-26 14:07:47 +0100

    add local_test folder

---
+++ local_test/params_gauss_error.py
@@ -0,0 +1,154 @@
+# -----------------------------
+# Description of the simulation
+# -----------------------------
+# Please fill in a verbal description of the simulation. 
+# It will be printed at the beginning of the simulation and can be used to keep track of the different runs.
+
+description = f"""
+This is the default simulation for the model VlasovMaxwellOneSpecies. 
+It is meant to be a template for users to set up their own simulations with this model. 
+It contains all the necessary components of a Struphy simulation, including the model, 
+the environment options, the time stepping options, the geometry, the equilibrium, 
+the grid, the Derham options, and the initial conditions. 
+Users can modify this file to set up their own simulations with different parameters and initial conditions.
+"""
+
+# ------------------
+# Import Struphy API
+# ------------------
+
+from struphy import (
+    BaseUnits,
+    DerhamOptions,
+    EnvironmentOptions,
+    FieldsBackground,
+    Simulation,
+    Time,
+    domains,
+    equils,
+    grids,
+    perturbations,
+)
+
+# For particles:
+from struphy import (
+    BinningPlot,
+    BoundaryParameters,
+    KernelDensityPlot,
+    LoadingParameters,
+    WeightsParameters,
+    maxwellians,
+)
+
+# ---------------------
+# Instance of the model
+# ---------------------
+
+from struphy.models import VlasovMaxwellOneSpecies
+model = VlasovMaxwellOneSpecies()
+
+# List all species and set their physical properties (charge and mass number, etc.)
+model.em_fields.set_species_properties()
+model.kinetic_ions.set_species_properties()
+
+# List all variables and decide whether to save their data
+model.em_fields.e_field.save_data = True
+model.em_fields.b_field.save_data = True
+model.em_fields.phi.save_data = True
+model.kinetic_ions.var.save_data = True
+model.measure_gauss_error(measure = True)
+
+# --------------------------
+# Instance of the simulation
+# --------------------------
+
+# Environment options
+env = EnvironmentOptions(sim_folder="cvT")
+
+# Units
+base_units = BaseUnits()
+
+# Time stepping
+time_opts = Time(Tend = 2)
+
+# Geometry
+domain = domains.Cuboid()
+
+# Fluid equilibrium (can be used as part of initial conditions)
+equil = equils.HomogenSlab()
+
+# Grid
+grid = grids.TensorProductGrid(Nel = (24, 1, 1))
+
+# Derham options
+derham_opts = DerhamOptions()
+
+# Simulation object
+sim = Simulation(
+    model=model,
+    params_path=__file__,
+    env=env,
+    base_units=base_units,
+    time_opts=time_opts,
+    domain=domain,
+    equil=equil,
+    grid=grid,
+    derham_opts=derham_opts,
+)
+
+# -------------------
+# Particle parameters
+# -------------------
+
+loading_params = LoadingParameters(ppc = 1_000, seed = 1234)
+weights_params = WeightsParameters(control_variate = True)
+boundary_params = BoundaryParameters()
+model.kinetic_ions.set_markers(loading_params=loading_params,
+                               weights_params=weights_params,
+                               boundary_params=boundary_params,
+                               )
+model.kinetic_ions.set_sorting_boxes()
+
+binplot = BinningPlot(slice='e1', n_bins=128, ranges=(0.0, 1.0))
+model.kinetic_ions.set_save_data(binning_plots=(binplot,))
+
+# ------------------
+# Propagator options
+# ------------------
+
+model.propagators.maxwell.options = model.propagators.maxwell.Options()
+model.propagators.push_eta.options = model.propagators.push_eta.Options()
+model.propagators.push_vxb.options = model.propagators.push_vxb.Options(b2_var=model.em_fields.b_field)
+model.propagators.coupling_va.options = model.propagators.coupling_va.Options()
+model.initial_poisson.options = model.initial_poisson.Options()
+
+# ------------------
+# Initial conditions
+# ------------------
+# Initial conditions are the sum of the background(s) and the perturbation(s).
+# If backgrounds or perturbations are not specified, they are assumed to be zero.
+
+# Background for (some) FEEC variables
+model.em_fields.phi.add_background(FieldsBackground())
+
+# Perturbations for (some) FEEC variables
+model.em_fields.phi.add_perturbation(perturbations.TorusModesCos())
+
+# For kinetic species the background is mandatory.
+# For kinetic species, if add_initial_condition() is not called, the background is taken as the kinetic initial condition.
+# For kinetic species the perturbations are added to the moments of the distribution function (defined as tuples).
+
+# Background for kinetic species
+maxwellian_1 = maxwellians.Maxwellian3D(n=(1.0, None))
+maxwellian_2 = maxwellians.Maxwellian3D(n=(0.1, None))
+background = maxwellian_1 + maxwellian_2
+model.kinetic_ions.var.add_background(background)
+
+# Perturbations for (some) kinetic species
+perturbation = perturbations.TorusModesCos()
+maxwellian_1pt = maxwellians.Maxwellian3D(n=(1.0, perturbation))
+init = maxwellian_1pt + maxwellian_2
+model.kinetic_ions.var.add_initial_condition(init)
+
+if __name__ == "__main__":
+    sim.run(verbose=True)
---
+++ local_test/pproc_gauss_error.py
@@ -0,0 +1,54 @@
+import params_gauss_error as damping_params
+
+from struphy.physics.physics import Units
+from struphy import PostProcessor, PlottingData
+
+import os
+import cunumpy as xp
+import h5py
+from feectools.ddm.mpi import mpi as MPI
+from matplotlib import pyplot as plt
+
+### Get Parameters ###
+
+dt = damping_params.time_opts.dt
+algo = damping_params.time_opts.split_algo
+Nel = damping_params.grid.Nel
+p = damping_params.derham_opts.p
+control_variate = damping_params.weights_params.control_variate
+
+
+env = damping_params.env
+ppc = damping_params.loading_params.ppc
+
+# get scalar data (post-processing not required)
+if MPI.COMM_WORLD.Get_rank() == 0:
+    pa_data = os.path.join(env.path_out, "data")
+    with h5py.File(os.path.join(pa_data, "data_proc0.hdf5"), "r") as f:
+        time = f["time"]["value"][()]
+        gauss_error = f["scalar"]["gauss_error"][()]
+        E_energy = f["scalar"]["en_E"][()]
+        B_energy = f["scalar"]["en_B"][()]
+
+    # plot
+    fig, axs = plt.subplots(nrows = 2, ncols = 1, figsize = (10,6), sharex = True)
+
+    axs[0].plot(time, gauss_error, color = "black")
+    axs[0].set_title("Gauss law violation")
+    axs[0].set_ylim(1e-1,1e4)
+    axs[0].set_ylabel("residual [a.u.]")
+
+    axs[1].plot(time, E_energy, label = r"$|E|^2/2$", color = "red")
+    axs[1].plot(time, B_energy, label = r"$|B|^2/2$", color = "blue")
+    axs[1].set_ylim(1e-27,1e9)
+    axs[1].set_xlim(0,2)
+    axs[1].set_title("Energy in EM field")
+    axs[1].set_ylabel("Energy [a.u.]")
+    axs[1].set_xlabel("time")
+
+    for ax in axs: ax.grid(); ax.set_yscale("log"); ax.legend(loc = "lower right")
+
+    fig.suptitle(f"VlasovMaxwellOneSpecies simulation:\n {control_variate=}, {ppc=}, {algo=}")
+    plt.tight_layout()
+    plt.savefig(f"gaussError_{control_variate=}")
+    plt.show()
