9fc2188555758b32c0e28932a8d02eaa0e9bcd24
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-03-02 17:39:02 +0100

    re-add another if verbose (for the moment)

--- src/struphy/simulation/sim.py
+++ src/struphy/simulation/sim.py
@@ -646,7 +646,7 @@ RESTARTing from:
                 self.data.save_data(keys=save_keys_all)
 
                 # print current time and scalar quantities to screen
-                if self.rank == 0:
+                if self.rank == 0 and verbose:
                     step = str(self.time_state["index"][0]).zfill(len(total_steps))
 
                     message = "time step:".ljust(25) + f"{step}/{total_steps}".rjust(25)
