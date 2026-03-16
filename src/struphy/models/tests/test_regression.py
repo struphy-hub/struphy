13135c9c680a3b38ec18a3c22f5a3b513e334eb1
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-03-16 11:50:36 +0100

    fomratting

--- src/struphy/models/tests/test_regression.py
+++ src/struphy/models/tests/test_regression.py
@@ -1,6 +1,7 @@
-import pytest
-from pathlib import Path
 import shutil
+from pathlib import Path
+
+import pytest
 from feectools.ddm.mpi import mpi as MPI
 
 from struphy.io.setup import import_parameters_py
@@ -8,9 +9,9 @@ from struphy.io.setup import import_parameters_py
 EXAMPLES_DIR = Path(__file__).resolve().parents[4] / "examples"
 
 PARAMS_MODULES = sorted(
-    p for p in EXAMPLES_DIR.rglob("*.py") if p.name != "__init__.py"
-    and p.name.startswith("params_")
+    p for p in EXAMPLES_DIR.rglob("*.py") if p.name != "__init__.py" and p.name.startswith("params_")
 )
+
 
 @pytest.mark.regression
 @pytest.mark.parametrize("params_path", PARAMS_MODULES)
@@ -31,29 +32,29 @@ def test_examples(params_path: Path):
     """
     rel = params_path.relative_to(EXAMPLES_DIR).with_suffix("")
     example_name = rel.parts[-1]
-    
+
     pproc_name = example_name.replace("params_", "pproc_")
     pproc_path = params_path / f"{pproc_name}.py"
-    
+
     regress_name = example_name.replace("params_", "regress_")
     regress_path = params_path / f"{regress_name}.py"
-    
+
     print("\nTesting example:", example_name)
     print(f"{params_path = }")
     print(f"{pproc_path = }")
     print(f"{regress_path = }")
-    
+
     params = import_parameters_py(str(params_path), name=example_name)
     params.sim.run(verbose=False)
-    
+
     MPI.COMM_WORLD.Barrier()
     if MPI.COMM_WORLD.Get_rank() == 0:
         if pproc_path.exists():
             pproc_module = import_parameters_py(str(pproc_path), name=pproc_name)
             pproc_module.main()
-            
+
         if regress_path.exists():
             regress_module = import_parameters_py(str(regress_path), name=regress_name)
             regress_module.main()
-            
+
     shutil.rmtree(params.sim.env.path_out)
