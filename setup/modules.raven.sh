a1c55fbd026d0264bf41dea053f705b14d66ebbc
Max <max.lindqvist@ipp.mpg.de>
2026-03-20 15:07:48 +0100

    Install struphy on clusters (#155)
    - [x] Added private runners on TOK, Raven, Viper, and Pitagora.
    - [x] Add script for loading the recommended module files for each
    cluster
    - [x] Add CI job which installs the code on each cluster
    - [x] Only run the CI job in a weekly scheduled pipeline (?)
    
    The `.github/scripts/install_and_test.sh` script is executed only on the
    private runners, it installs, compiles and tests struphy. It can be
    executed manually too and should work on Raven/Viper/TOK/Pitagora
    
    To simplify the installation on the clusters, I've added the following
    bash files in `setup/`:
    
    - `modules.pitagora.sh`
    - `modules.raven.sh`
    - `modules.tok.sh`
    - `modules.viper.sh`
    - `modules.sh`
    
    The `modules.MACHINE` contain the variables `MODULES_INTEL` and
    `MODULES_GCC` which lists the modules needed for intel/gcc on the
    current machine.
    
    The `modules.sh` detects which machine we are on, and helps you load or
    show the modules needed.
    
    ```
    # Usage:
    #   ./modules.sh [load|display]
    #     load    - Loads the required modules for the detected system/compiler
    #     display - Prints the module load command for the detected system/compiler
    ```
    
    So for example, if you want to load the GCC modules (which is default),
    you just type:
    
    ```
    source ./setup/modules.sh load
    # or
    source ./setup/modules.sh load gcc
    ```
    
    To load the intel modules, you type:
    
    ```
    source ./setup/modules.sh load intel
    ```

---
+++ setup/modules.raven.sh
@@ -0,0 +1,10 @@
+MODULES_INTEL="intel/21.7.1 \
+impi/2021.7 \
+python-waterboa/2024.06 \
+mkl/2025.3 \
+hdf5-serial/1.14.1"
+
+MODULES_GCC="gcc/15 \
+openmpi/5.0 \
+python-waterboa/2025.06 \
+pandoc/3.1"
