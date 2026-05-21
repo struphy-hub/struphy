5a980bafc28f280d5a14f6fe33047f193c4d0de7
emile grivet <emilegrivet@gmail.com>
2026-05-21 20:35:59 +0200

    writing of the test for the operator in mass.py

--- src/struphy/feec/mass.py
+++ src/struphy/feec/mass.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 
 import cunumpy as xp
 from feectools.api.settings import PSYDAC_BACKEND_GPYCCEL
+from feectools.ddm.mpi import MockComm
 from feectools.ddm.mpi import mpi as MPI
 from feectools.fem.tensor import TensorFemSpace
 from feectools.fem.vector import VectorFemSpace
@@ -11,7 +12,6 @@ from feectools.linalg.basic import IdentityOperator, LinearOperator, Vector
 from feectools.linalg.block import BlockLinearOperator, BlockVector
 from feectools.linalg.solvers import inverse
 from feectools.linalg.stencil import StencilDiagonalMatrix, StencilMatrix, StencilVector
-from feectools.ddm.mpi import MockComm
 
 from struphy import domains, equils
 from struphy.feec import mass_kernels
@@ -2924,47 +2924,48 @@ class AverageOperator(LinOpWithTransp):
     .. math::
 
         \mathbb M^{\alpha}_{(\mu,ijk),(\nu,mno)} = \delta_{i,m} \delta_{j,n} c_o
-    
+
         with :math:`c_k=\int_0^1 N_{3,k}(\eta_3) \textnormal{d} \eta` and :math:`N_{3,k}` the B-spline function at the place `k`.
 
     Parameters
     ----------
     derham : Derham
         The derham complexe that supports the space used
-    
+
     space : str
         Identifier of the space on which the average is performed, either `"H1"`, `"Hcurl"`, `"Hdiv"`, `"L2"`, `"H1vec"`
 
     direction : int, optional
         The direction of the space along which the average is performed, either `0`, `1` or `2`.
-    
+
     transposed : bool, optional
         Whether to take the transpose of the operator.
     """
+
     def __init__(
-            self,
-            derham: Derham,
-            space: str = "H1",
-            direction: int = 2,
-            transposed: bool = False,
-            ):
+        self,
+        derham: Derham,
+        space: str = "H1",
+        direction: int = 2,
+        transposed: bool = False,
+    ):
 
         if not space in derham.space_to_form:
             AssertionError("Must match a space of the derham complex")
         if space != "H1":
             NotImplementedError()
         space_id = "V" + derham.space_to_form[space]
-        self._V = getattr(derham, space_id) #StencilVectorSpace
+        self._V = getattr(derham, space_id)  # StencilVectorSpace
         self._domain = getattr(derham, space_id)
         self._codomain = getattr(derham, space_id)
-        self._pads = self._V.pads #gets the number of ghost cells
+        self._pads = self._V.pads  # gets the number of ghost cells
         self._derham = derham
         self._dtype = self._domain.dtype
         self._transposed = transposed
         if direction == 0:
             self._directions = (0, 1, 2)
         elif direction == 1:
-            self._directions = (1, 2, 0)
+            self._directions = (1, 0, 2)
         elif direction == 2:
             self._directions = (2, 0, 1)
         else:
@@ -2983,14 +2984,40 @@ class AverageOperator(LinOpWithTransp):
             self.subcomm = comm.Split(color=color, key=rank)
 
         # We allocate memory for the 2D temporary array for each process
-        self._tmp = xp.zeros((int(self._V.ends[self._directions[1]] - self._V.starts[self._directions[1]] + 1),
-                                int(self._V.ends[self._directions[2]] - self._V.starts[self._directions[2]] + 1)))
+        self._tmp = xp.zeros(
+            (
+                int(self._V.ends[self._directions[1]] - self._V.starts[self._directions[1]] + 1),
+                int(self._V.ends[self._directions[2]] - self._V.starts[self._directions[2]] + 1),
+            )
+        )
 
         # We allocate memory for the weights (integrals of 1D B-splines)
         self._weights = xp.zeros(int(self._V.ends[self._directions[0]] - self._V.starts[self._directions[0]] + 1))
-        
+
         self.allocate()
-    
+
+        # definition of subscripts for function xp.einsum
+        if self._transposed:
+            if self._directions[0] == 0:
+                self._subscripts = ("ijk->jk", "ij,o->oij")
+            if self._directions[0] == 1:
+                self._subscripts = ("ijk->ik", "ij,o->ioj")
+            if self._directions[0] == 2:
+                self._subscripts = ("ijk->ij", "ij,o->ijo")
+        else:
+            if self._directions[0] == 0:
+                self._subscripts = ("ojk,o->jk",)
+            if self._directions[0] == 1:
+                self._subscripts = ("iok,o->ik",)
+            if self._directions[0] == 2:
+                self._subscripts = ("ijo,o->ij",)
+
+        # definition of slices
+        sl_ghost = tuple(slice(p, -p) if p > 0 else slice(None) for p in self._pads)
+        sl_broadcasting = [slice(None), slice(None), slice(None)]
+        sl_broadcasting[self._directions[0]] = None
+        self._slices = (sl_ghost, tuple(sl_broadcasting))
+
     def allocate(self):
         """Compute the weights, which are the integrals of 1D B-splines in the averaged direction"""
         knots = getattr(self.derham.args_derham, "tn" + str(self._directions[0] + 1))
@@ -3001,7 +3028,7 @@ class AverageOperator(LinOpWithTransp):
             i_begin += self.derham.degree[self._directions[0]]
             i_end += self.derham.degree[self._directions[0]]
         # General formula for any distribution of knots for the integral of a B-spline function, thus works with periodic and clamped boundary conditions :
-        self._weights[:] = (knots[i_begin+degree+1:i_end+degree+1] - knots[i_begin:i_end]) / (degree+1)
+        self._weights[:] = (knots[i_begin + degree + 1 : i_end + degree + 1] - knots[i_begin:i_end]) / (degree + 1)
 
     @property
     def domain(self):
@@ -3010,11 +3037,11 @@ class AverageOperator(LinOpWithTransp):
     @property
     def codomain(self):
         return self._codomain
-    
+
     @property
     def dtype(self):
         return self._dtype
-    
+
     @property
     def derham(self):
         return self._derham
@@ -3033,55 +3060,31 @@ class AverageOperator(LinOpWithTransp):
     @property
     def toarray(self):
         raise NotImplementedError()
-    
+
     def dot(self, v, out=None):
 
-        #assert isinstance(v, StencilVector)
-        #assert v.space == self.domain
+        # assert isinstance(v, StencilVector)
+        # assert v.space == self.domain
 
         v.update_ghost_regions()
 
         if out is None:
             out = self.codomain.zeros()
 
-        sl = tuple(
-        slice(p, -p) if p > 0 else slice(None)
-        for p in self._pads
-        )
-
-        x = v._data[sl]
-        y = out._data[sl]
+        x = v._data[self._slices[0]]
+        y = out._data[self._slices[0]]
         if self._transposed:
-            xp.einsum(
-                "ijk->ij",
-                x,
-                out=self._tmp
-            )
+            xp.einsum(self._subscripts[0], x, out=self._tmp)
             if not isinstance(self.derham.comm, (MockComm, type(None))):
                 self.subcomm.Allreduce(MPI.IN_PLACE, self._tmp, MPI.SUM)
-            xp.einsum(
-                "ij,o->ijo",
-                self._tmp,
-                self._weights,
-                out=y
-            )
+            xp.einsum(self._subscripts[1], self._tmp, self._weights, out=y)
         else:
-            xp.einsum(
-                "ijo,o->ij",
-                x,
-                self._weights,
-                out=self._tmp
-            )
+            xp.einsum(self._subscripts[0], x, self._weights, out=self._tmp)
             if not isinstance(self.derham.comm, (MockComm, type(None))):
                 self.subcomm.Allreduce(MPI.IN_PLACE, self._tmp, MPI.SUM)
-            y[:] = self._tmp[:,:,None]
+            y[:] = self._tmp[self._slices[1]]
 
         return out
 
     def transpose(self, conjugate=False):
-        return AverageOperator(
-            self.derham,
-            self.domain,
-            self._weights,
-            transposed = not self._transposed
-        )
+        return AverageOperator(self.derham, self.domain, self._weights, transposed=not self._transposed)
