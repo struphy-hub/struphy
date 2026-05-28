1dd1b27252e56076497a3d12600c67d74e73177e
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-05-28 12:34:26 +0200

    forgot to save a file

--- src/struphy/propagators/current_coupling_5d_curlb.py
+++ src/struphy/propagators/current_coupling_5d_curlb.py
@@ -167,7 +167,7 @@ class CurrentCoupling5DCurlb(Propagator):
         logger.info(f"\nNew options for propagator '{self.__class__.__name__}':\n{self._options}")
 
     @profile
-    def allocate(self, verbose: bool = False):
+    def allocate(self):
         if self.options.u_space == "H1vec":
             self._u_form_int = 0
         else:
