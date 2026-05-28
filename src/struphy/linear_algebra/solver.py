f39db85754a512d87f01c9d4bee70fe7881647ed
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-05-28 08:17:07 +0200

    refactor L2Projector: add type annotations, change signature to accept SolverParameters.

--- src/struphy/linear_algebra/solver.py
+++ src/struphy/linear_algebra/solver.py
@@ -29,8 +29,12 @@ class DiscreteGradientSolverParameters:
     relaxation_factor: float = 0.5
     tol: float = 1e-12
     maxiter: int = 20
-    verbose: bool = False
     info: bool = False
+    
+    def __post_init__(self):
+        self.verbose = False
+        if logger.level <= logging.DEBUG:
+            self.verbose = True
 
 
 @dataclass
@@ -40,6 +44,10 @@ class NonlinearSolverParameters:
     tol: float = 1e-8
     maxiter: int = 100
     info: bool = False
-    verbose: bool = False
     type: LiteralOptions.OptsNonlinearSolver = "Picard"
     linearize: bool = False
+    
+    def __post_init__(self):
+        self.verbose = False
+        if logger.level <= logging.DEBUG:
+            self.verbose = True
