d549ed71f8dc8f327044cc3efff4f86f73fe8884
emile grivet <emilegrivet@gmail.com>
2026-06-25 15:54:19 +0200

    adition of ITG test case

---
+++ examples/drift_kinetic_model/ITG_test_case/params_drift_kinetic.py
@@ -0,0 +1,236 @@
+# -----------------------------
+# Description of the simulation
+# -----------------------------
+# Please fill in a verbal description of the simulation. 
+# It will be printed at the beginning of the simulation and can be used to keep track of the different runs.
+
+name = "Default DriftKineticElectrostaticAdiabatic"
+description = """
+This is the default simulation for the model DriftKineticElectrostaticAdiabatic. 
+It is meant to be a template for users to set up their own simulations with this model. 
+It contains all the necessary components of a Struphy simulation, including the model, 
+the environment options, the time stepping options, the geometry, the equilibrium, 
+the grid, the Derham options, and the initial conditions. 
+Users can modify this file to set up their own simulations with different parameters and initial conditions.
+"""
+
+import logging
+from struphy import set_logging_level
+set_logging_level(logging.INFO)
+
+import cunumpy as xp
+
+# ------------------
+# Import Struphy API
+# ------------------
+
+from struphy.propagators import implicit_diffusion
+
+from struphy import (
+    BaseUnits,
+    DerhamOptions,
+    EnvironmentOptions,
+    FieldsBackground,
+    Simulation,
+    Time,
+    domains,
+    equils,
+    grids,
+    perturbations,
+)
+
+# For particles:
+from struphy import (
+    BinningPlot,
+    BoundaryParameters,
+    KernelDensityPlot,
+    LoadingParameters,
+    WeightsParameters,
+    maxwellians,
+    SortingParameters,
+    SavingParameters,
+)
+
+# ---------------------
+# Instance of the model
+# ---------------------
+
+from struphy.models import DriftKineticElectrostaticAdiabatic
+
+restart = False
+
+# Units
+base_units = BaseUnits(kBT=1.0)
+
+# Model instance
+model = DriftKineticElectrostaticAdiabatic(base_units=base_units, epsilon=1.0, alpha=1.0)
+
+# List all variables and decide whether to save their data
+model.em_fields.phi.save_data = True
+model.kinetic_ions.var.save_data = False
+
+# --------------------------
+# Instance of the simulation
+# --------------------------
+
+# Environment options
+env = EnvironmentOptions(sim_folder="sim_1", profiling_activated=True, profiling_trace=True)
+
+# Time stepping
+time_opts = Time(dt=1.0, Tend=1200.0, split_algo="LieTrotter")
+
+# Geometry
+a1, a2, Lz = 0.1, 14.5, 1506.759067
+domain = domains.HollowCylinder(a1=a1, a2=a2, Lz=Lz)
+
+# Fluid equilibrium (can be used as part of initial conditions)
+equil = equils.HomogenSlab(B0x=0.0, B0y=0.0, B0z=1.0)
+
+# Grid
+Nx, Ny, Nz = (42,96,18)
+num_element = (Nx, Ny, Nz)
+grid = grids.TensorProductGrid(num_elements=num_element, mpi_dims_mask=(True,True,True))
+
+# Derham options
+derham_opts = DerhamOptions(degree=(2,2,2), bcs=(("dirichlet", "dirichlet"), None, None))
+
+# Simulation object
+sim = Simulation(
+    model=model,
+    name=name,
+    description=description,
+    params_path=__file__,
+    env=env,
+    time_opts=time_opts,
+    domain=domain,
+    equil=equil,
+    grid=grid,
+    derham_opts=derham_opts,
+)
+
+# -------------------
+# Particle parameters
+# -------------------
+
+Np = 5000000
+loading_params = LoadingParameters(Np=Np, loading="pseudo_random", spatial="uniform", seed=1234)
+weights_params = WeightsParameters(control_variate=True)
+boundary_params = BoundaryParameters(bc=('remove','periodic','periodic'))
+sorting_params = SortingParameters(
+    do_sort=True,
+    boxes_per_dim=num_element,
+    sorting_frequency=0,
+)
+
+binplot = BinningPlot(slice='e1_e2', n_bins=(64,128), ranges=((0.0, 1.0), (0.0, 1.0)))
+saving_params = SavingParameters(binning_plots=(binplot,))
+
+model.kinetic_ions.set_markers(loading_params=loading_params,
+                               weights_params=weights_params,
+                               boundary_params=boundary_params,
+                               sorting_params=sorting_params,
+                               saving_params=saving_params,
+                               bufsize=2.0,
+                               )
+
+# ------------------
+# Propagator options
+# ------------------
+
+model.propagators.gc_poisson.options.solver_params = implicit_diffusion.SolverParameters(maxiter=5000, info=True)
+model.propagators.push_gc_bxe.options = model.propagators.push_gc_bxe.Options(phi=model.em_fields.phi, algo="explicit", evaluate_e_field=True)
+model.propagators.push_gc_para.options = model.propagators.push_gc_para.Options(phi=model.em_fields.phi, algo="explicit", evaluate_e_field=True)
+
+# ------------------
+# Initial conditions
+# ------------------
+# Initial conditions are the sum of the background(s) and the perturbation(s).
+# If backgrounds or perturbations are not specified, they are assumed to be zero.
+
+# Background for (some) FEEC variables
+#model.em_fields.phi.add_background(FieldsBackground(values=(0.0,)))
+
+# Perturbations for (some) FEEC variables
+#model.em_fields.phi.add_perturbation(perturbations.TorusModesCos())
+
+# For kinetic species the background is mandatory.
+# For kinetic species, if add_initial_condition() is not called, the background is taken as the kinetic initial condition.
+# For kinetic species the perturbations are added to the moments of the distribution function (defined as tuples).
+
+# Background for kinetic species
+
+
+rp = (a2+a1)/2
+amps=1e-6
+ms, ns = 5, 0
+kappa_n0 = 0.055
+kappa_Ti = kappa_Te = 0.27586
+delta_r_Ti = delta_r_Te = 1.45
+delta_r_n0 = delta_r_Ti / 2
+delta_r = 4 * delta_r_n0 / delta_r_Ti
+C_Ti = C_Te = 1.0
+N_integrate = 1000000
+C_n0 = (a2-a1) / xp.sum(xp.exp(-kappa_n0*delta_r_n0*xp.tanh((xp.linspace(a1,a2,N_integrate)-rp)/delta_r_n0))*(a2-a1)/N_integrate)
+
+def n0(r):
+    return C_n0 * xp.exp(-kappa_n0*delta_r_n0*xp.tanh((r-rp)/delta_r_n0))
+from struphy.initial.base import GenericPerturbation
+
+def n_init(*etas):
+    if len(etas)==1:
+        eta1=etas[0][:,0]
+    else:
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
+    return n0(r)
+
+def pert_func(*etas):
+    if len(etas)==1:
+        e1,e2,e3 = etas[0][:,0], etas[0][:,1], etas[0][:,1]
+    else:
+        e1, e2, e3 = etas[0], etas[1], etas[2]
+    r = (a1 + (a2 - a1) * e1)
+    teta = 2*xp.pi * e2
+    z = Lz * e3
+    return n0(r)*amps*xp.exp(-(r-rp)**2/delta_r**2)*xp.cos(2*xp.pi*ns*z/Lz + ms*teta)
+
+def Ti(r):
+    return C_Ti * xp.exp(-kappa_Ti*delta_r_Ti*xp.tanh((r-rp)/delta_r_Ti))
+
+def vth_i(*etas):
+    if len(etas)==1:
+        eta1=etas[0][:,0]
+    else:
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
+    return xp.sqrt(Ti(r))
+
+def vth_e(*etas):
+    if len(etas)==1:
+        eta1=etas[0][:,0]
+    else:
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
+    return xp.sqrt(Ti(r))
+
+def n0_xyz(x, y, z):
+    r = xp.sqrt(x**2 + y**2)
+    return n0(r)
+
+def p_xyz(x, y, z):
+    r = xp.sqrt(x**2 + y**2)
+    return n0(r)*Ti(r)
+
+equil.p_xyz = p_xyz
+equil.n_xyz = n0_xyz
+
+
+perturbation = GenericPerturbation(pert_func)
+background = maxwellians.GyroMaxwellian2D(n=(n_init, None), equil=equil, vth_para=(vth_i,None), vth_perp=(vth_i,None))
+#background.plot_density_profile(dim_1="e1", dim_2="e2", in_physical=True, domain=domain, resol=100, integrate_resol=10, logical_coord=(0.0,0.0,0.0), plot_3D=False, use_mu=True)
+model.kinetic_ions.var.add_background(background)
+init = maxwellians.GyroMaxwellian2D(n=(n_init, perturbation), equil=equil, vth_para=(vth_i,None), vth_perp=(vth_i,None))
+model.kinetic_ions.var.add_initial_condition(init)
+
+if __name__ == "__main__":
+    sim.run(verbose=True, one_time_step=False)
---
+++ examples/drift_kinetic_model/ITG_test_case/pproc_drift_kinetic.py
@@ -0,0 +1,187 @@
+import importlib.util
+import pyvista as pv
+from struphy import PlottingData, PostProcessor
+
+import os
+import sys
+import cunumpy as xp
+from matplotlib import pyplot as plt
+import h5py
+
+
+# ------------------
+# Post process simulation data
+# ------------------
+def main():
+    if len(sys.argv)>1 and __name__=="__main__":
+        sim_name = sys.argv[1]
+    else:
+        sim_name = "sim_1"
+    sim_path = os.path.join(os.getcwd(), sim_name)
+
+    spec = importlib.util.spec_from_file_location("params", os.path.join(sim_path, "parameters.py"))
+    params = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(params)
+
+    if not os.path.isdir(os.path.join(sim_path, "post_processing")):
+        pp = PostProcessor(sim=params.sim)
+        pp.process(physical=True)
+
+    pdata = PlottingData(sim=params.sim)
+    pdata.load()
+
+    equil_data = pv.read(os.path.join(sim_path, "geometry.vts"))
+
+    # path to save plots
+    # save_path = os.path.join(os.getcwd(), "images", "sim")
+    # os.makedirs(save_path, exist_ok=True)
+
+    # ------------------
+    # Check simulation domain
+    # ------------------
+
+    #params.domain.show()
+
+    # get scalar data (post processing not needed for scalar data)
+    pa_data = os.path.join(sim_path, "data")
+    with h5py.File(os.path.join(pa_data, "data_proc0.hdf5"), "r") as f:
+        time = f["time"]["value"][()]
+        en_phi = f["scalar"]["phi_integral"][()]#"en_phi"
+    
+    t0, t1 = 10, 50
+    m, b = xp.polyfit(time[t0:t1], xp.log(xp.sqrt(en_phi))[t0:t1], 1)
+
+
+    plt.figure()
+    plt.plot(time, xp.sqrt(en_phi), label="simulated value")
+    plt.plot(time[t0:t1], xp.exp(time[t0:t1]* m + b), label=f"fit y=c*e^(mx) with {m=} and c={xp.exp(b)}")
+    plt.legend()
+    plt.semilogy()
+    plt.show()
+
+    equil_dim_grid = equil_data.dimensions
+    equil_grid = xp.reshape(equil_data.points, equil_dim_grid + (3,))
+    equil_r = xp.sqrt(equil_grid[:,:,:,0]**2 + equil_grid[:,:,:,1]**2)
+    equil_p0 = xp.reshape(equil_data.point_data["p0"], equil_dim_grid)
+    if "n0" in equil_data.point_data:
+         equil_n0 = xp.reshape(equil_data.point_data["n0"], equil_dim_grid)
+    plt.figure()
+    plt.title("radial distribution")
+    plt.xlabel("r")
+    plt.plot(equil_r[0,0,:], equil_p0[0,0,:], label="p0")
+    if "n0" in equil_data.point_data:
+        plt.plot(equil_r[0,0,:], equil_n0[0,0,:], label="n0")
+        plt.plot(equil_r[0,0,:], equil_p0[0,0,:]/equil_n0[0,0,:], label="T0")
+    plt.legend()
+    plt.show()
+
+    nrows = 4
+    ncols = 4
+    ntime = len(pdata.f.kinetic_ions.e1_e2_density.f_binned) 
+    time_indices = [int( i/(nrows*ncols-1) * (ntime - 1) ) for i in range(nrows*ncols)]
+
+    def plot_radial_density(bin_name, quantity, x_label = "r", y_label = "density"):
+        time_idx=0
+        fig, axs = plt.subplots(nrows = nrows, ncols = ncols, figsize = (14,10), sharex=True, sharey=True)
+        for i in range(nrows):
+            for j in range(ncols):
+                ax_maxwellian = axs[i][j]
+                previous = time_idx
+                time_idx = time_indices[j + i*ncols]
+
+                #maxwellian distribution plot
+                f = getattr(
+                    getattr(pdata.f.kinetic_ions, bin_name), quantity
+                    )[time_idx]
+                f2 = getattr(
+                    getattr(pdata.f.kinetic_ions, bin_name), quantity
+                    )[previous]
+
+                pcm = ax_maxwellian.plot(f[:,0])# - f2[:,0])
+
+                ax_maxwellian.set_xlabel(x_label)
+                ax_maxwellian.set_ylabel(y_label)
+                ax_maxwellian.set_title(f"t = {pdata.t_grid[time_idx]:4.2e}")
+        fig.suptitle(quantity)
+        plt.tight_layout()
+        plt.show()
+
+    plot_radial_density("e1_e2_density", "delta_f_binned")
+
+    def plot_phaseSpace(bin_name, quantity, xs, ys, x_label = "x", y_label = "y", in_physical = False):
+
+            fig, axs = plt.subplots(nrows = nrows, ncols = ncols, figsize = (14,10), sharex=True, sharey=True)
+            for i in range(nrows):
+                for j in range(ncols):
+                    ax_maxwellian = axs[i][j]
+                    time_idx = time_indices[j + i*ncols]
+
+                    #maxwellian distribution plot
+                    color_mapped = getattr(
+                        getattr(pdata.f.kinetic_ions, bin_name), quantity
+                        )[time_idx].T
+
+                    if in_physical: color_mapped = color_mapped.T
+
+                    pcm = ax_maxwellian.pcolor(xs, ys, color_mapped)
+
+                    ax_maxwellian.set_xlabel(x_label)
+                    ax_maxwellian.set_ylabel(y_label)
+                    ax_maxwellian.set_title(f"t = {pdata.t_grid[time_idx]:4.2e}")
+                    fig.colorbar(pcm, ax = ax_maxwellian)
+            fig.suptitle(quantity)
+            plt.tight_layout()
+            plt.show()
+    print(f"evol : {xp.max([xp.abs(pdata.f.kinetic_ions.e1_e2_density.f_binned[i+1]-pdata.f.kinetic_ions.e1_e2_density.f_binned[i]) for i in range(ntime-1)])}")
+    e1_bin = pdata.f.kinetic_ions.e1_e2_density.grid_e1
+    e2_bin = pdata.f.kinetic_ions.e1_e2_density.grid_e2
+    phy_bin = params.domain(e1_bin, e2_bin, 0, squeeze_out=True)
+    plot_phaseSpace("e1_e2_density", "delta_f_binned", xs=phy_bin[0], ys=phy_bin[1], in_physical=True)
+
+
+    # ------------------
+    # Show evolution of electric potential
+    # ------------------
+    time_indices = [int( i/(nrows*ncols-1) * (ntime - 1) ) for i in range(nrows*ncols)]
+
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(14,10), sharex=True, sharey=True)
+
+    for i in range(nrows):
+        for j in range(ncols):
+            ax_maxwellian = axs[i][j]
+            time_idx = time_indices[j + i*ncols]
+
+            phi = pdata.spline_values.em_fields.phi_phy.data[pdata.t_grid[time_idx]][0][:,:,0]
+
+            pcm = ax_maxwellian.pcolormesh(pdata.grids_phy[0][:,:,0], pdata.grids_phy[1][:,:,0], phi)
+
+            ax_maxwellian.set_xlabel("x")
+            ax_maxwellian.set_ylabel("y")
+            ax_maxwellian.set_title(f"Electrical potential at t = {pdata.t_grid[time_idx]:4.2e}")
+
+            fig.colorbar(pcm, ax=ax_maxwellian)
+
+    plt.tight_layout()
+    plt.show()
+
+    save_video_pngs = False
+    if save_video_pngs:
+        if not os.path.exists(sim_path+"/video"):
+            os.mkdir(sim_path+"/video")
+        # create .png for video
+        jump = 1
+        fig = plt.figure(figsize=(8, 8))
+        for n in range(50):
+            if n % jump == 0:
+                color_mapped = pdata.f.kinetic_ions.e1_e2_density.f_binned[n].T
+                plt.pcolor(phy_bin[0], phy_bin[1], pdata.f.kinetic_ions.e1_e2_density.delta_f_binned[n])
+                
+                plt.xlabel("x position")
+                plt.ylabel("y position")
+                plt.title(f"t = {pdata.t_grid[n]:4.2e}")
+                plt.savefig(sim_path+"/video"+f"/fig_{n:04.0f}.png", transparent=False, bbox_inches='tight', pad_inches=0)
+
+
+
+if __name__ == "__main__":
+    main()
