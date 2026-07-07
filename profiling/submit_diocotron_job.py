334294fcc9f11396d80ac58b06184655f1c1867f
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2026-07-07 10:27:15 +0200

    Removed TOK commadn

--- profiling/submit_diocotron_job.py
+++ profiling/submit_diocotron_job.py
@@ -65,7 +65,7 @@ def main() -> None:
     cases = [
         ProfilingCase(
             name="diocotron_poisson_scaling",
-            command="python /toks/work/maxlin/git_repos/struphy/profiling/run_diocotron.py {nranks}",
+            command="python profiling/run_diocotron.py {nranks}",
             ranks=(1, 2, 4, 8),
         ),
     ]
