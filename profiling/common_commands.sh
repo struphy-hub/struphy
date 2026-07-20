b0541f95ef8e91566a41e362d9a9f7e454d40a79
Max <max.lindqvist@ipp.mpg.de>
2026-07-20 12:04:14 +0200

    Don't compile in profiling/common_commands.sh

--- profiling/common_commands.sh
+++ profiling/common_commands.sh
@@ -66,11 +66,4 @@ comm = MPI.COMM_WORLD
 print(f'Hello from rank {comm.Get_rank()} of {comm.Get_size()}')
 assert comm.Get_size() == 4
 "
-echo
 
-echo "----------------------------------------"
-echo "Compiling kernels with Pyccel"
-echo "----------------------------------------"
-pyccel --version
-struphy compile -y --language "$LANGUAGE"
-echo
