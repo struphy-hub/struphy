9f715cffa882e7eb7fd7672476e32af9f395f7c3
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-07-21 19:55:24 +0200

    sort imports

--- src/struphy/models/__init__.py
+++ src/struphy/models/__init__.py
@@ -10,6 +10,7 @@ from struphy.models.deterministic_particle_diffusion import DeterministicParticl
 from struphy.models.drift_kinetic_electrostatic_adiabatic import DriftKineticElectrostaticAdiabatic
 from struphy.models.guiding_center import GuidingCenter
 from struphy.models.hasegawa_wakatani import HasegawaWakatani
+from struphy.models.incompressible_navier_stokes_sph import IncompressibleNavierStokesSPH
 from struphy.models.linear_extended_mh_duniform import LinearExtendedMHDuniform
 from struphy.models.linear_mhd import LinearMHD
 from struphy.models.linear_mhd_driftkinetic_cc import LinearMHDDriftkineticCC
@@ -39,7 +40,6 @@ from struphy.models.viscous_fluid import ViscousFluid
 from struphy.models.vlasov import Vlasov
 from struphy.models.vlasov_ampere_one_species import VlasovAmpereOneSpecies
 from struphy.models.vlasov_maxwell_one_species import VlasovMaxwellOneSpecies
-from struphy.models.incompressible_navier_stokes_sph import IncompressibleNavierStokesSPH
 
 __all__ = [
     "ColdPlasma",
