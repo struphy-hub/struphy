59060bd550d2c80bfed9869e6dfcfa019dd9067b
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-07-22 10:53:07 +0200

    print particles domain array if derham is None

--- src/struphy/simulation/sim.py
+++ src/struphy/simulation/sim.py
@@ -490,7 +490,14 @@ class Simulation(SimulationBase):
 
         # print info on mpi procs
         if self.comm_size < 32:
-            logger.info(f"\nderham.domain_array:\n{self.derham.domain_array}")
+            if self.derham is not None:
+                logger.info(f"\nderham.domain_array:\n{self.derham.domain_array}")
+            else:
+                for _, species in self.model.species.items():
+                    for _, variable in species.variables.items():
+                        if isinstance(variable, (PICVariable, SPHVariable)):
+                            logger.info(f"\nparticle domain_array:\n{variable.particles.domain_array}")
+                            break
 
         if self.rank < 32:
             logger.debug("")
