ffca5aa990eea3814e71ffd00cd6e73d08f3b3a3
Max <max.lindqvist@ipp.mpg.de>
2026-07-23 14:17:58 +0200

    Resolve 171 init files are not formatted at the moment (#286)
    **Solves the following issue(s):**
    
    Closes #171, #172.
    
    Reformatted the init files and added a `struphy build-init-files` to
    rebuild them after adding new models or propagators.
    
    In the near future, I think we can completely get rid of the `struphy
    format` command, and just use `ruff format` for formatting and `ruff
    check` for linting instead.

--- src/struphy/propagators/__init__.py
+++ src/struphy/propagators/__init__.py
@@ -1,93 +1,91 @@
-# from struphy.propagators.propagators_coupling import (
-#     CurrentCoupling5DCurlb,
-#     CurrentCoupling5DGradB,
-#     CurrentCoupling6DCurrent,
-#     EfieldWeights,
-#     PressureCoupling6D,
-#     VlasovAmpere,
-# )
-# from struphy.propagators.propagators_fields import (
-#     AdiabaticPhi,
-#     CurrentCoupling5DDensity,
-#     CurrentCoupling6DDensity,
-#     FaradayExtended,
-#     Hall,
-#     HasegawaWakatani,
-#     ImplicitDiffusion,
-#     JxBCold,
-#     Magnetosonic,
-#     MagnetosonicUniform,
-#     Maxwell,
-#     OhmCold,
-#     Poisson,
-#     ShearAlfven,
-#     ShearAlfvenB1,
-#     ShearAlfvenCurrentCoupling5D,
-#     TimeDependentSource,
-#     TwoFluidQuasiNeutralFull,
-#     VariationalDensityEvolve,
-#     VariationalEntropyEvolve,
-#     VariationalMagFieldEvolve,
-#     VariationalMomentumAdvection,
-#     VariationalPBEvolve,
-#     VariationalQBEvolve,
-#     VariationalResistivity,
-#     VariationalViscosity,
-# )
-# from struphy.propagators.propagators_markers import (
-#     PushDeterministicDiffusion,
-#     PushEta,
-#     PushEtaPC,
-#     PushGuidingCenterBxEstar,
-#     PushGuidingCenterParallel,
-#     PushRandomDiffusion,
-#     PushVinEfield,
-#     PushVinSPHpressure,
-#     PushVinViscousPotential,
-#     PushVxB,
-# )
+from struphy.propagators.adiabatic_phi import AdiabaticPhi
+from struphy.propagators.curl_curl_solve import CurlCurlSolve
+from struphy.propagators.current_coupling_5d_curlb import CurrentCoupling5DCurlb
+from struphy.propagators.current_coupling_5d_density import CurrentCoupling5DDensity
+from struphy.propagators.current_coupling_5d_gradb import CurrentCoupling5DGradB
+from struphy.propagators.current_coupling_6d_current import CurrentCoupling6DCurrent
+from struphy.propagators.current_coupling_6d_density import CurrentCoupling6DDensity
+from struphy.propagators.efield_weights_coupling import EfieldWeightsCoupling
+from struphy.propagators.faraday_extended import FaradayExtended
+from struphy.propagators.hall import Hall
+from struphy.propagators.hasegawa_wakatani_step import HasegawaWakataniStep
+from struphy.propagators.implicit_diffusion import ImplicitDiffusion
+from struphy.propagators.jxb_cold import JxBCold
+from struphy.propagators.magnetosonic import Magnetosonic
+from struphy.propagators.magnetosonic_uniform import MagnetosonicUniform
+from struphy.propagators.maxwell_weak_ampere import MaxwellWeakAmpere
+from struphy.propagators.ohm_cold import OhmCold
+from struphy.propagators.poisson_adiabatic_gyrokinetic import PoissonAdiabaticGyrokinetic
+from struphy.propagators.poisson_solve import PoissonSolve
+from struphy.propagators.pressure_coupling_6d import PressureCoupling6D
+from struphy.propagators.push_deterministic_diffusion import PushDeterministicDiffusion
+from struphy.propagators.push_eta import PushEta
+from struphy.propagators.push_eta_pc import PushEtaPC
+from struphy.propagators.push_guiding_center_bx_estar import PushGuidingCenterBxEstar
+from struphy.propagators.push_guiding_center_parallel import PushGuidingCenterParallel
+from struphy.propagators.push_random_diffusion import PushRandomDiffusion
+from struphy.propagators.push_vin_efield import PushVinEfield
+from struphy.propagators.push_vin_sph_pressure import PushVinSPHpressure
+from struphy.propagators.push_vin_viscous_potential import PushVinViscousPotential
+from struphy.propagators.push_vxb import PushVxB
+from struphy.propagators.shear_alfven_b1 import ShearAlfvenB1
+from struphy.propagators.shear_alfven_current_coupling_5d import ShearAlfvenCurrentCoupling5D
+from struphy.propagators.shear_alfven_propagator import ShearAlfvenPropagator
+from struphy.propagators.time_dependent_source import TimeDependentSource
+from struphy.propagators.two_fluid_quasi_neutral_full import TwoFluidQuasiNeutralFull
+from struphy.propagators.variational_density_evolve import VariationalDensityEvolve
+from struphy.propagators.variational_entropy_evolve import VariationalEntropyEvolve
+from struphy.propagators.variational_mag_field_evolve import VariationalMagFieldEvolve
+from struphy.propagators.variational_momentum_advection import VariationalMomentumAdvection
+from struphy.propagators.variational_pb_evolve import VariationalPBEvolve
+from struphy.propagators.variational_qb_evolve import VariationalQBEvolve
+from struphy.propagators.variational_resistivity import VariationalResistivity
+from struphy.propagators.variational_viscosity import VariationalViscosity
+from struphy.propagators.vlasov_ampere_coupling import VlasovAmpereCoupling
 
-# __all__ = [
-#     "VlasovAmpere",
-#     "EfieldWeights",
-#     "PressureCoupling6D",
-#     "CurrentCoupling6DCurrent",
-#     "CurrentCoupling5DCurlb",
-#     "CurrentCoupling5DGradB",
-#     "Maxwell",
-#     "OhmCold",
-#     "JxBCold",
-#     "ShearAlfven",
-#     "ShearAlfvenB1",
-#     "Hall",
-#     "Magnetosonic",
-#     "MagnetosonicUniform",
-#     "FaradayExtended",
-#     "CurrentCoupling6DDensity",
-#     "ShearAlfvenCurrentCoupling5D",
-#     "CurrentCoupling5DDensity",
-#     "ImplicitDiffusion",
-#     "Poisson",
-#     "VariationalMomentumAdvection",
-#     "VariationalDensityEvolve",
-#     "VariationalEntropyEvolve",
-#     "VariationalMagFieldEvolve",
-#     "VariationalPBEvolve",
-#     "VariationalQBEvolve",
-#     "VariationalViscosity",
-#     "VariationalResistivity",
-#     "TimeDependentSource",
-#     "AdiabaticPhi",
-#     "HasegawaWakatani",
-#     "TwoFluidQuasiNeutralFull",
-#     "PushEta",
-#     "PushVxB",
-#     "PushVinEfield",
-#     "PushEtaPC",
-#     "PushGuidingCenterBxEstar",
-#     "PushGuidingCenterParallel",
-#     "PushDeterministicDiffusion",
-#     "PushRandomDiffusion",
-#     "PushVinSPHpressure",
-#     "PushVinViscousPotential",
-# ]
+__all__ = [
+    "AdiabaticPhi",
+    "CurlCurlSolve",
+    "CurrentCoupling5DCurlb",
+    "CurrentCoupling5DDensity",
+    "CurrentCoupling5DGradB",
+    "CurrentCoupling6DCurrent",
+    "CurrentCoupling6DDensity",
+    "EfieldWeightsCoupling",
+    "FaradayExtended",
+    "Hall",
+    "HasegawaWakataniStep",
+    "ImplicitDiffusion",
+    "JxBCold",
+    "Magnetosonic",
+    "MagnetosonicUniform",
+    "MaxwellWeakAmpere",
+    "OhmCold",
+    "PoissonAdiabaticGyrokinetic",
+    "PoissonSolve",
+    "PressureCoupling6D",
+    "PushDeterministicDiffusion",
+    "PushEta",
+    "PushEtaPC",
+    "PushGuidingCenterBxEstar",
+    "PushGuidingCenterParallel",
+    "PushRandomDiffusion",
+    "PushVinEfield",
+    "PushVinSPHpressure",
+    "PushVinViscousPotential",
+    "PushVxB",
+    "ShearAlfvenB1",
+    "ShearAlfvenCurrentCoupling5D",
+    "ShearAlfvenPropagator",
+    "TimeDependentSource",
+    "TwoFluidQuasiNeutralFull",
+    "VariationalDensityEvolve",
+    "VariationalEntropyEvolve",
+    "VariationalMagFieldEvolve",
+    "VariationalMomentumAdvection",
+    "VariationalPBEvolve",
+    "VariationalQBEvolve",
+    "VariationalResistivity",
+    "VariationalViscosity",
+    "VlasovAmpereCoupling",
+]
