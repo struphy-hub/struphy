48e2f0efb5b4bcdd679407d0751c6b22d9d7bec3
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2026-07-28 10:05:48 +0200

    diocotron up to 256 cores again

--- profiling/submit_diocotron_strong_scaling.py
+++ profiling/submit_diocotron_strong_scaling.py
@@ -45,7 +45,7 @@ def main() -> None:
     )
 
     # Launch one run per rank count
-    for num_tasks in (2, 4): #, 8, 16, 32, 64, 128, 256):
+    for num_tasks in (2, 4, 8, 16, 32, 64, 128, 256):
         profiling_case.launch(num_tasks)
 
     # Wait for all jobs to finish, and then build the comparison plot and package the case.
