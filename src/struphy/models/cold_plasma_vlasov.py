8f002cd768ccddcac8356e19580236cca88917cd
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-08-03 09:39:02 +0200

    implement fix of weights that Anish identified

--- src/struphy/models/cold_plasma_vlasov.py
+++ src/struphy/models/cold_plasma_vlasov.py
@@ -218,6 +218,9 @@ class ColdPlasmaVlasov(StruphyModel):
         phi = self.initial_poisson.variables.phi.spline.vector
         Propagator.derham.grad.dot(-phi, out=self.em_fields.e_field.spline.vector)
         logger.info("... Done.")
+        
+        # reset particle weights
+        particles.weights = particles.weights_at_t0.copy()
 
     ## default parameters
     def generate_default_parameter_file(self, path=None, prompt=True):
