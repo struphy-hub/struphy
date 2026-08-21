4dc0557caac374f458a3894a273394755bf18c33
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2026-08-21 13:48:23 +0200

    Restore examples to minimal changes

--- examples/DriftKineticElectrostaticAdiabatic/cyclone/params_cyclone.py
+++ examples/DriftKineticElectrostaticAdiabatic/cyclone/params_cyclone.py
@@ -1,7 +1,7 @@
 # -----------------------------
 # Description of the simulation
 # -----------------------------
-# Please fill in a verbal description of the simulation.
+# Please fill in a verbal description of the simulation. 
 # It will be printed at the beginning of the simulation and can be used to keep track of the different runs.
 
 name = "Cyclone instability"
@@ -23,49 +23,49 @@ DOI: 10.1140/epjd/e2014-50180-9
 
 
 import logging
-
 from struphy import set_logging_level
-
 set_logging_level(logging.INFO)
 
-import cunumpy as xp
-
-# For particles:
 from struphy import (
     BaseUnits,
-    BinningPlot,
-    BoundaryParameters,
     DerhamOptions,
     EnvironmentOptions,
     FieldsBackground,
-    KernelDensityPlot,
-    LoadingParameters,
-    SavingParameters,
     Simulation,
-    SortingParameters,
     Time,
-    WeightsParameters,
     domains,
     equils,
     grids,
-    maxwellians,
     perturbations,
 )
+
+# For particles:
+from struphy import (
+    BinningPlot,
+    BoundaryParameters,
+    KernelDensityPlot,
+    LoadingParameters,
+    WeightsParameters,
+    SortingParameters,
+    SavingParameters,
+    maxwellians,
+)
+
+import cunumpy as xp
 
 # ---------------------
 # Instance of the model
 # ---------------------
+
 from struphy.linear_algebra.solver import SolverParameters
 from struphy.models import DriftKineticElectrostaticAdiabatic
 from struphy.pic.accumulation.filter import FilterParameters
 
-base_units = BaseUnits(
-    kBT=0.1916
-)  # provides the correct value for epsilon = 1.4142e-3 = 0.36/(180*sqrt(2)) from the paper
+base_units = BaseUnits(kBT=0.1916) # provides the correct value for epsilon = 1.4142e-3 = 0.36/(180*sqrt(2)) from the paper
 model = DriftKineticElectrostaticAdiabatic(
     base_units=base_units,
     use_diagnostic_poisson=True,
-)
+    )
 
 # List all variables and decide whether to save their data
 model.em_fields.phi.save_data = True
@@ -82,26 +82,24 @@ env = EnvironmentOptions(sim_folder="sim_1", restart=False)
 time_opts = Time(dt=0.001, Tend=0.01, split_algo="LieTrotter")
 
 a, r_min, R0 = 0.36, 0.01, 1.0
-num_elements = (32, 5 * 27, 5)
+num_elements = (32, 5*27, 5)
 degree = (3, 3, 3)
 
 # Fluid equilibrium (can be used as part of initial conditions)
-equil = equils.AdhocTorus(a=a, R0=R0, B0=1.0, q_kind=2, q0=0.86, q1=2.52 + 0.86, l=-0.16, psi_k=5, psi_nel=200)
+equil = equils.AdhocTorus(a=a, R0=R0, B0=1.0, q_kind=2, q0=0.86, q1=2.52+0.86, l=-0.16, psi_k=5, psi_nel=200)
 
 # Geometry
 # domain = domains.Tokamak(equil, num_elements=num_elements[:2], degree=degree[:2], r_min=r_min, num_elements_pre=(128, 512), p_pre=(4, 4), xi_param="sfl", tor_period=19)
-domain = domains.HollowTorus(
-    a1=r_min, a2=a, R0=R0, sfl=True, pol_period=1, tor_period=19
-)  # use a hollowtorus to avoid premaping
+domain = domains.HollowTorus(a1=r_min, a2=a, R0=R0, sfl=True, pol_period=1, tor_period=19) # use a hollowtorus to avoid premaping
 
 # Grid
-grid = grids.TensorProductGrid(num_elements=num_elements, mpi_dims_mask=(True, True, False))
+grid = grids.TensorProductGrid(num_elements=num_elements, mpi_dims_mask=(True,True,False))
 
 # Derham options
 derham_opts = DerhamOptions(
-    degree=degree,
+    degree=degree, 
     bcs=(("dirichlet", "dirichlet"), None, None),
-)
+    )
 
 
 # Simulation object
@@ -122,45 +120,41 @@ sim = Simulation(
 # Particle parameters
 # -------------------
 
-ppc = 50  # run with 200 minimum
-loading_params = LoadingParameters(ppc=ppc, loading="sobol_standard", spatial="uniform", moments=(0, 0, 4, 4))
+ppc = 50 # run with 200 minimum
+loading_params = LoadingParameters(ppc = ppc, loading="sobol_standard", spatial="uniform", moments=(0, 0, 4, 4))
 weights_params = WeightsParameters(control_variate=True)
 boundary_params = BoundaryParameters(bc=("remove", "periodic", "periodic"))
-sorting_params = SortingParameters(boxes_per_dim=(12, 12, 6), do_sort=True, sorting_frequency=5)
+sorting_params = SortingParameters(boxes_per_dim=(12,12,6), do_sort=True, sorting_frequency=5)
 
 # density binning
-eta_bin = BinningPlot(slice="e1_e2", n_bins=(64, 64), ranges=((0.01, 0.99), (0.0, 1.0)))
-eta_bin2 = BinningPlot(slice="e2_e3", n_bins=(64, 64), ranges=((0.0, 1.0), (0.0, 1.0)))
+eta_bin = BinningPlot(slice='e1_e2', n_bins= (64,64), ranges= ((0.01, 0.99), (0.0, 1.0)))
+eta_bin2 = BinningPlot(slice='e2_e3', n_bins= (64,64), ranges= ((0.0, 1.0), (0.0, 1.0)))
 saving_params = SavingParameters(n_markers=100, binning_plots=(eta_bin,))
 
-model.kinetic_ions.set_markers(
-    loading_params=loading_params,
-    weights_params=weights_params,
-    boundary_params=boundary_params,
-    sorting_params=sorting_params,
-    saving_params=saving_params,
-    bufsize=1.0,
-)
+model.kinetic_ions.set_markers(loading_params=loading_params,
+                               weights_params=weights_params,
+                               boundary_params=boundary_params,
+                               sorting_params=sorting_params,
+                               saving_params=saving_params,
+                               bufsize=1.0,
+                               )
 
 # ------------------
 # Propagator options
 # ------------------
 
-model.propagators.gc_poisson.options = model.propagators.gc_poisson.Options(
-    which_geometry="toroidal",
-    solver_params=SolverParameters(tol=1e-12, maxiter=3000, recycle=False),
-    filter_params={model.kinetic_ions.var: FilterParameters("fourier_in_tor", (1,), repeat=1)},
-)
-model.propagators.push_gc_bxe.options = model.propagators.push_gc_bxe.Options(
-    algo="explicit",
-    evaluate_e_field=True,
-    maxiter=100,
-)
-model.propagators.push_gc_para.options = model.propagators.push_gc_para.Options(
-    algo="explicit",
-    evaluate_e_field=True,
-    maxiter=100,
-)
+model.propagators.gc_poisson.options = model.propagators.gc_poisson.Options(which_geometry="toroidal",
+                                                                            solver_params=SolverParameters(tol=1e-12,maxiter=3000, recycle=False),
+                                                                            filter_params={model.kinetic_ions.var: FilterParameters("fourier_in_tor", (1,), repeat=1)},
+                                                                            )
+model.propagators.push_gc_bxe.options = model.propagators.push_gc_bxe.Options(algo="explicit", 
+                                                                              evaluate_e_field=True, 
+                                                                              maxiter=100,
+                                                                              )
+model.propagators.push_gc_para.options = model.propagators.push_gc_para.Options(algo="explicit", 
+                                                                                evaluate_e_field=True, 
+                                                                                maxiter=100,
+                                                                                )
 
 # ------------------
 # Initial conditions
@@ -184,77 +178,59 @@ r0 = 0.5 * a
 n0 = 1.0
 Ti0 = 1.0
 
-
 def n_r(r):
-    return n0 * xp.exp(-kappa_n * a * Delta_n * xp.tanh((r - r0) / (Delta_n * a)))
-
+    return n0 * xp.exp(-kappa_n*a*Delta_n*xp.tanh((r-r0)/(Delta_n*a)))
 
 def n_init(*etas):
-    if len(etas) == 1:
-        eta1 = etas[0][:, 0]
+    if len(etas)==1:
+        eta1=etas[0][:,0]
     else:
-        eta1 = etas[0]
+        eta1=etas[0]
     r = r_min + (a - r_min) * eta1
     return n_r(r)
 
-
 def Ti_r(r):
-    return Ti0 * xp.exp(-kappa_Ti * a * Delta_Ti * xp.tanh((r - r0) / (Delta_Ti * a)))
-
+    return Ti0 * xp.exp(-kappa_Ti*a*Delta_Ti*xp.tanh((r-r0)/(Delta_Ti*a)))
 
 def vth_init(*etas):
-    if len(etas) == 1:
-        eta1 = etas[0][:, 0]
+    if len(etas)==1:
+        eta1=etas[0][:,0]
     else:
-        eta1 = etas[0]
+        eta1=etas[0]
     r = r_min + (a - r_min) * eta1
     return xp.sqrt(Ti_r(r))
 
-
-def n_xyz(x, y, z):
-    r = xp.sqrt((xp.sqrt(x**2 + y**2) - R0) ** 2 + z**2)
+def n_xyz(x,y,z):
+    r = xp.sqrt((xp.sqrt(x**2 + y**2)-R0)**2 + z**2)
     return n_r(r)
 
-
-def p_xyz(x, y, z):
-    r = xp.sqrt((xp.sqrt(x**2 + y**2) - R0) ** 2 + z**2)
-    return n_r(r) * Ti_r(r)
-
+def p_xyz(x,y,z):
+    r = xp.sqrt((xp.sqrt(x**2 + y**2)-R0)**2 + z**2)
+    return n_r(r)*Ti_r(r)
 
 equil.p_xyz = p_xyz
 equil.n_xyz = n_xyz
 
-
 def pert_func(*etas):
-    if len(etas) == 1:
-        e1, e2, e3 = etas[0][:, 0], etas[0][:, 1], etas[0][:, 2]
+    if len(etas)==1:
+        e1,e2,e3 = etas[0][:,0], etas[0][:,1], etas[0][:,2]
     else:
         e1, e2, e3 = etas[0], etas[1], etas[2]
     r = (a - r_min) * e1 + r_min
-    teta = 2 * xp.arctan(xp.sqrt((R0 + r) / (R0 - r)) * xp.tan(xp.pi * e2))
-    phi = 2 * xp.pi * e3
-    return n_r(r) * amps * xp.exp(-((r - r0) ** 2) / delta_r**2) * xp.cos(ms * teta - ns * phi)
-
+    teta = 2*xp.arctan(xp.sqrt((R0+r)/(R0-r))*xp.tan(xp.pi*e2))
+    phi = 2*xp.pi * e3
+    return n_r(r)*amps*xp.exp(-(r-r0)**2/delta_r**2)*xp.cos(ms*teta - ns*phi)
 
 # Background for kinetic species
-background = maxwellians.GyroMaxwellian2D(
-    n=(n_init, None),
-    vth_para=(vth_init, None),
-    vth_perp=(vth_init, None),
-)  # B0=equil.absB0)
+background = maxwellians.GyroMaxwellian2D(n=(n_init, None), vth_para=(vth_init, None), vth_perp=(vth_init, None),)# B0=equil.absB0)
 model.kinetic_ions.var.add_background(background)
-# background.plot_density_profile("e1", "e2", domain=domain, plot_3D=True, in_physical=True)
-# background.plot_density_profile("e1", "v1", domain=domain)
-# background.plot_density_profile("e1", "v2", domain=domain, use_mu=True, equil=equil)
+#background.plot_density_profile("e1", "e2", domain=domain, plot_3D=True, in_physical=True)
+#background.plot_density_profile("e1", "v1", domain=domain)
+#background.plot_density_profile("e1", "v2", domain=domain, use_mu=True, equil=equil)
 
 from struphy.initial.base import GenericPerturbation
-
 perturbation = GenericPerturbation(pert_func, given_in_basis="0")
-init = maxwellians.GyroMaxwellian2D(
-    n=(n_init, perturbation),
-    vth_para=(vth_init, None),
-    vth_perp=(vth_init, None),
-)  # B0=equil.absB0)
+init = maxwellians.GyroMaxwellian2D(n=(n_init, perturbation), vth_para=(vth_init, None), vth_perp=(vth_init, None),)# B0=equil.absB0)
 model.kinetic_ions.var.add_initial_condition(init)
 
 if __name__ == "__main__":
--- examples/DriftKineticElectrostaticAdiabatic/itg_cylindre/params_drift_kinetic.py
+++ examples/DriftKineticElectrostaticAdiabatic/itg_cylindre/params_drift_kinetic.py
@@ -1,7 +1,7 @@
 # -----------------------------
 # Description of the simulation
 # -----------------------------
-# Please fill in a verbal description of the simulation.
+# Please fill in a verbal description of the simulation. 
 # It will be printed at the beginning of the simulation and can be used to keep track of the different runs.
 
 name = "Default DriftKineticElectrostaticAdiabatic"
@@ -15,44 +15,47 @@ Users can modify this file to set up their own simulations with different parame
 """
 
 import logging
-
 from struphy import set_logging_level
-
 set_logging_level(logging.INFO)
 
 import cunumpy as xp
 
-# For particles:
+# ------------------
+# Import Struphy API
+# ------------------
+
+from struphy.propagators import implicit_diffusion
+
 from struphy import (
     BaseUnits,
-    BinningPlot,
-    BoundaryParameters,
     DerhamOptions,
     EnvironmentOptions,
     FieldsBackground,
-    KernelDensityPlot,
-    LoadingParameters,
-    SavingParameters,
     Simulation,
-    SortingParameters,
     Time,
-    WeightsParameters,
     domains,
     equils,
     grids,
-    maxwellians,
     perturbations,
+)
+
+# For particles:
+from struphy import (
+    BinningPlot,
+    BoundaryParameters,
+    KernelDensityPlot,
+    LoadingParameters,
+    WeightsParameters,
+    maxwellians,
+    SortingParameters,
+    SavingParameters,
 )
 
 # ---------------------
 # Instance of the model
 # ---------------------
-from struphy.models import DriftKineticElectrostaticAdiabatic
 
-# ------------------
-# Import Struphy API
-# ------------------
-from struphy.propagators import implicit_diffusion
+from struphy.models import DriftKineticElectrostaticAdiabatic
 
 restart = False
 
@@ -64,7 +67,7 @@ model = DriftKineticElectrostaticAdiabatic(
     base_units=base_units,
     epsilon=1.0,
     use_diagnostic_poisson=True,
-)
+    )
 
 # List all variables and decide whether to save their data
 model.em_fields.phi.save_data = True
@@ -88,16 +91,15 @@ domain = domains.HollowCylinder(a1=a1, a2=a2, Lz=Lz)
 equil = equils.HomogenSlab(B0x=0.0, B0y=0.0, B0z=1.0)
 
 # Grid
-Nx, Ny, Nz = (32, 5 * 10, 10)
+Nx, Ny, Nz = (32, 5*10, 10)
 num_element = (Nx, Ny, Nz)
-grid = grids.TensorProductGrid(num_elements=num_element, mpi_dims_mask=(True, True, True))
+grid = grids.TensorProductGrid(num_elements=num_element, mpi_dims_mask=(True,True,True))
 
 # Derham options
-derham_opts = DerhamOptions(degree=(3, 3, 3), bcs=(("dirichlet", "dirichlet"), None, None))
+derham_opts = DerhamOptions(degree=(3,3,3), bcs=(("dirichlet", "dirichlet"), None, None))
 
 # Simulation object
 from feectools.ddm.mpi import mpi as MPI
-
 rank = MPI.COMM_WORLD.Get_rank()
 sim = Simulation(
     model=model,
@@ -116,27 +118,26 @@ sim = Simulation(
 # Particle parameters
 # -------------------
 
-ppc = 50  # run with 200 minimum
+ppc = 50 # run with 200 minimum
 loading_params = LoadingParameters(ppc=ppc, loading="sobol_standard", spatial="uniform", moments=(0.0, 0.0, 2.0, 2.0))
 weights_params = WeightsParameters(control_variate=True)
-boundary_params = BoundaryParameters(bc=("remove", "periodic", "periodic"))
+boundary_params = BoundaryParameters(bc=('remove','periodic','periodic'))
 sorting_params = SortingParameters(
     do_sort=True,
     boxes_per_dim=(12, 12, 6),
     sorting_frequency=0,
 )
 
-binplot = BinningPlot(slice="e1_e2", n_bins=(64, 128), ranges=((0.0, 1.0), (0.0, 1.0)))
+binplot = BinningPlot(slice='e1_e2', n_bins=(64,128), ranges=((0.0, 1.0), (0.0, 1.0)))
 saving_params = SavingParameters(binning_plots=(binplot,))
 
-model.kinetic_ions.set_markers(
-    loading_params=loading_params,
-    weights_params=weights_params,
-    boundary_params=boundary_params,
-    sorting_params=sorting_params,
-    saving_params=saving_params,
-    bufsize=2.0,
-)
+model.kinetic_ions.set_markers(loading_params=loading_params,
+                               weights_params=weights_params,
+                               boundary_params=boundary_params,
+                               sorting_params=sorting_params,
+                               saving_params=saving_params,
+                               bufsize=2.0,
+                               )
 
 # ------------------
 # Propagator options
@@ -153,10 +154,10 @@ model.propagators.push_gc_para.options = model.propagators.push_gc_para.Options(
 # If backgrounds or perturbations are not specified, they are assumed to be zero.
 
 # Background for (some) FEEC variables
-# model.em_fields.phi.add_background(FieldsBackground(values=(0.0,)))
+#model.em_fields.phi.add_background(FieldsBackground(values=(0.0,)))
 
 # Perturbations for (some) FEEC variables
-# model.em_fields.phi.add_perturbation(perturbations.TorusModesCos())
+#model.em_fields.phi.add_perturbation(perturbations.TorusModesCos())
 
 # For kinetic species the background is mandatory.
 # For kinetic species, if add_initial_condition() is not called, the background is taken as the kinetic initial condition.
@@ -165,8 +166,8 @@ model.propagators.push_gc_para.options = model.propagators.push_gc_para.Options(
 # Background for kinetic species
 
 
-rp = (a2 + a1) / 2
-amps = 1e-6
+rp = (a2+a1)/2
+amps=1e-6
 ms, ns = 5, 1
 kappa_n0 = 0.055
 kappa_Ti = kappa_Te = 0.27586
@@ -175,88 +176,65 @@ delta_r_n0 = delta_r_Ti / 2
 delta_r = 4 * delta_r_n0 / delta_r_Ti
 C_Ti = C_Te = 1.0
 N_integrate = 1000000
-C_n0 = (a2 - a1) / xp.sum(
-    xp.exp(-kappa_n0 * delta_r_n0 * xp.tanh((xp.linspace(a1, a2, N_integrate) - rp) / delta_r_n0))
-    * (a2 - a1)
-    / N_integrate
-)
-
+C_n0 = (a2-a1) / xp.sum(xp.exp(-kappa_n0*delta_r_n0*xp.tanh((xp.linspace(a1,a2,N_integrate)-rp)/delta_r_n0))*(a2-a1)/N_integrate)
 
 def n0(r):
-    return C_n0 * xp.exp(-kappa_n0 * delta_r_n0 * xp.tanh((r - rp) / delta_r_n0))
-
-
+    return C_n0 * xp.exp(-kappa_n0*delta_r_n0*xp.tanh((r-rp)/delta_r_n0))
 from struphy.initial.base import GenericPerturbation
 
-
 def n_init(*etas):
-    if len(etas) == 1:
-        eta1 = etas[0][:, 0]
+    if len(etas)==1:
+        eta1=etas[0][:,0]
     else:
-        eta1 = etas[0]
-    r = a1 + (a2 - a1) * eta1
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
     return n0(r)
 
-
 def pert_func(*etas):
-    if len(etas) == 1:
-        e1, e2, e3 = etas[0][:, 0], etas[0][:, 1], etas[0][:, 2]
+    if len(etas)==1:
+        e1,e2,e3 = etas[0][:,0], etas[0][:,1], etas[0][:,2]
     else:
         e1, e2, e3 = etas[0], etas[1], etas[2]
-    r = a1 + (a2 - a1) * e1
-    teta = 2 * xp.pi * e2
+    r = (a1 + (a2 - a1) * e1)
+    teta = 2*xp.pi * e2
     z = Lz * e3
-    return n0(r) * amps * xp.exp(-((r - rp) ** 2) / delta_r**2) * xp.cos(2 * xp.pi * ns * z / Lz + ms * teta)
-
+    return n0(r)*amps*xp.exp(-(r-rp)**2/delta_r**2)*xp.cos(2*xp.pi*ns*z/Lz + ms*teta)
 
 def Ti(r):
-    return C_Ti * xp.exp(-kappa_Ti * delta_r_Ti * xp.tanh((r - rp) / delta_r_Ti))
-
+    return C_Ti * xp.exp(-kappa_Ti*delta_r_Ti*xp.tanh((r-rp)/delta_r_Ti))
 
 def vth_i(*etas):
-    if len(etas) == 1:
-        eta1 = etas[0][:, 0]
+    if len(etas)==1:
+        eta1=etas[0][:,0]
     else:
-        eta1 = etas[0]
-    r = a1 + (a2 - a1) * eta1
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
     return xp.sqrt(Ti(r))
-
 
 def vth_e(*etas):
-    if len(etas) == 1:
-        eta1 = etas[0][:, 0]
+    if len(etas)==1:
+        eta1=etas[0][:,0]
     else:
-        eta1 = etas[0]
-    r = a1 + (a2 - a1) * eta1
+        eta1=etas[0]
+    r = (a1 + (a2 - a1) * eta1)
     return xp.sqrt(Ti(r))
-
 
 def n0_xyz(x, y, z):
     r = xp.sqrt(x**2 + y**2)
     return n0(r)
 
-
 def p_xyz(x, y, z):
     r = xp.sqrt(x**2 + y**2)
-    return n0(r) * Ti(r)
-
+    return n0(r)*Ti(r)
 
 equil.p_xyz = p_xyz
 equil.n_xyz = n0_xyz
 
 
 perturbation = GenericPerturbation(pert_func)
-background = maxwellians.GyroMaxwellian2D(
-    n=(n_init, None),
-    vth_para=(vth_i, None),
-    vth_perp=(vth_i, None),
-)  # B0=equil.absB0)
+background = maxwellians.GyroMaxwellian2D(n=(n_init, None), vth_para=(vth_i,None), vth_perp=(vth_i,None),)# B0=equil.absB0)
 model.kinetic_ions.var.add_background(background)
-init = maxwellians.GyroMaxwellian2D(
-    n=(n_init, perturbation),
-    vth_para=(vth_i, None),
-    vth_perp=(vth_i, None),
-)  # B0=equil.absB0)
+init = maxwellians.GyroMaxwellian2D(n=(n_init, perturbation), vth_para=(vth_i,None), vth_perp=(vth_i,None),)# B0=equil.absB0)
 model.kinetic_ions.var.add_initial_condition(init)
 
 if __name__ == "__main__":
