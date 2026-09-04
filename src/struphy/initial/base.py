6caf1db41bf3775507c51e8ac80ef92543d205a6
Max <max.lindqvist@ipp.mpg.de>
2026-09-04 17:56:25 +0200

    Improve docstring helpers (#361)

--- src/struphy/initial/base.py
+++ src/struphy/initial/base.py
@@ -4,7 +4,19 @@ from typing import Callable
 
 import cunumpy as xp
 
+try:
+    from IPython.display import HTML, display
+except ImportError:
+
+    def HTML(data):
+        return data
+
+    def display(*objects, **kwargs):
+        return objects[0] if objects else None
+
+
 from struphy.io.options import LiteralOptions
+from struphy.utils.docstring_converter import rst_to_html, rst_to_latex, rst_to_markdown
 from struphy.utils.utils import __class_with_params_repr_no_defaults__, check_option
 
 logger = logging.getLogger("struphy")
@@ -82,6 +94,28 @@ class Perturbation(metaclass=ABCMeta):
 
     def __repr_no_defaults__(self):
         return __class_with_params_repr_no_defaults__(self)
+
+    forced_heading_level = 5
+
+    @classmethod
+    def _doc_text(cls) -> str:
+        return cls.__doc__ if cls.__doc__ else "Description not available for this perturbation."
+
+    @classmethod
+    def doc_html(cls) -> str:
+        return rst_to_html(cls._doc_text(), forced_heading_level=cls.forced_heading_level)
+
+    @classmethod
+    def doc_markdown(cls) -> str:
+        return rst_to_markdown(cls._doc_text())
+
+    @classmethod
+    def doc_latex(cls) -> str:
+        return rst_to_latex(cls._doc_text())
+
+    @classmethod
+    def doc(cls):
+        return display(HTML(cls.doc_html()))
 
     @property
     def given_in_basis(self) -> str:
