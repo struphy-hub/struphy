d1a4e1747412c678e92705bde3345abd6cb13c5e
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-09-09 15:07:24 +0200

    new file test_verif_IncompressibleNavierStokesSPH.py from Claude - te… (#308)
    * Added three verification tests for the SPH discretization of
    incompressible Navier-Stokes.
    * Renamend propagator `PushVinEfield` -> `PushVinForceField`
    
    ---------
    
    Co-authored-by: aminrai2000 <sayyed.amin.raiessi.toussi@ipp.mpg.de>

--- src/struphy/fields_background/base.py
+++ src/struphy/fields_background/base.py
@@ -173,6 +173,7 @@ class FluidEquilibrium(metaclass=ABCMeta):
     @classmethod
     def formula(cls):
         return display(HTML(cls.formula_html()))
+
     ###########################
     # Vector-valued callables #
     ###########################
