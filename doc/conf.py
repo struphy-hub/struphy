96cf167f107ad3f078a3b296d661a8bb3fe8c189
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-09-11 13:45:01 +0200

    change doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,18 @@ extensions = [
     "sphinx_collections",
 ]
 
-nbsphinx_execute = "auto"
+def _struphy_is_compiled():
+    """Whether `struphy compile` has been run (state.yml exists in the installed package)."""
+    try:
+        from struphy.utils.utils import STRUPHY_LIBPATH
+    except ImportError:
+        return False
+    return os.path.isfile(os.path.join(STRUPHY_LIBPATH, "state.yml"))
+
+
+# Notebooks are slow to run and many depend on compiled Struphy kernels,
+# so only execute them when Struphy has been compiled; otherwise reuse stored outputs.
+nbsphinx_execute = "auto" if _struphy_is_compiled() else "never"
 nbsphinx_kernel_name = 'local-env' # This is just for Stefan's local machine, where the system kernel does not work.
 
 napoleon_use_admonition_for_examples = True
