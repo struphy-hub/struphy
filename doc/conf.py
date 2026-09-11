652cd2050f83956337bfa5d23608a94a31741ce9
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-09-11 08:57:07 +0200

    mock mpi4py import in RTD

--- doc/conf.py
+++ doc/conf.py
@@ -80,6 +80,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# mock imports
+autodoc_mock_imports = ["mpi4py"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
