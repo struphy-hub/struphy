cf2db0417d2887138a7eecdbe03b4944c97457a4
Max <max.lindqvist@ipp.mpg.de>
2026-09-20 16:12:07 +0200

    Add newline to timestep print

--- src/struphy/simulation/sim.py
+++ src/struphy/simulation/sim.py
@@ -831,7 +831,7 @@ class Simulation(SimulationBase):
                     # print current time and scalar quantities to screen
                     step = str(int(self.time_state["index"][0])).zfill(len(total_steps_str))
 
-                    message = "\n" + "-" * 80
+                    message = "\n" + "-" * 80 + "\n"
                     message += "time step:".ljust(25) + f"{step}/{total_steps + start_step}".rjust(25)
                     message += (
                         "\n"
