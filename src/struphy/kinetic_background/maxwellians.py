72efa3853d3fbdbf57a480539d6df0aa6525950b
Byung <bk.na.0327@gmail.com>
2026-09-24 02:08:49 +0100

    fix handling of velocity jacobian det

--- src/struphy/kinetic_background/maxwellians.py
+++ src/struphy/kinetic_background/maxwellians.py
@@ -214,7 +214,11 @@ class GyroMaxwellian2D(Maxwellian):
 
         B0 = self.params["B0"]
 
-        return B0(eta1, eta2, eta3) if callable(B0) else B0 + 0 * eta1
+        if callable(B0):
+            etas = xp.stack((eta1, eta2, eta3), axis=1)
+            return B0(etas)
+
+        return B0 + 0 * eta1
 
     @property
     def volume_form(self) -> bool:
@@ -504,15 +508,6 @@ class CanonicalMaxwellian2D(GyroMaxwellian2D):
     def epsilon(self) -> float:
         """Epsilon parameter in the canonical toroidal momentum."""
         return self._epsilon
-
-    def velocity_jacobian_det(self, eta1, eta2, eta3, v_para, mu):
-        r"""Jacobian determinant of the velocity coordinate transformation to :math:`(v_\parallel, mu)`, is :math:`B_0`.
-        """
-        assert eta1.ndim == eta2.ndim == eta3.ndim == 1
-        assert v_para.ndim == mu.ndim == 1
-
-        etas = xp.stack((eta1, eta2, eta3), axis=1)
-        return self.equil.absB0(etas)
 
     def _evaluate_moment(
         self,
